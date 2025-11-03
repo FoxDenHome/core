388b9b095036a75a272ac17fe2c65f0939dfebeb
Doridian <git@doridian.net>
2025-11-03 08:57:30 -0800

    iaid

--- nix/vms/bengalfox/win2k22/config.nix
+++ nix/vms/bengalfox/win2k22/config.nix
@@ -12,7 +12,7 @@
     mac = "5e:8c:f2:cd:c8:4a";
     dhcpv6 = {
       duid = "0x000100013078e51e5e8cf2cdc84a";
-      iaid = 106058752;
+      iaid = 451288365;
     };
     addresses = [
       "10.2.10.10/16"
