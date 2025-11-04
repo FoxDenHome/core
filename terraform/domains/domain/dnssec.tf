3f9e50ac68202d0acb1c0be6415792b98b61479b
Doridian <git@doridian.net>
2025-11-03 21:10:24 -0800

    dnskey not supported by cloudns

--- terraform/domains/domain/dnssec.tf
+++ terraform/domains/domain/dnssec.tf
@@ -9,16 +9,6 @@ locals {
   dnskey_records = toset(jsondecode(data.external.dnssec_json.result.dnskeys))
 }
 
-resource "cloudns_dns_record" "dnskey" {
-  zone     = cloudns_dns_zone.domain.id
-  for_each = { for r in local.dnskey_records : r => split(" ", r) }
-
-  type     = each.value[3]
-  name     = trimsuffix(trimsuffix(each.value[0], "${var.domain}."), ".")
-  ttl      = tonumber(each.value[1])
-  value    = "${each.value[4]} ${each.value[5]} ${each.value[6]} ${each.value[7]}"
-}
-
 output "dnskey_records" {
   value = local.dnskey_records
 }
