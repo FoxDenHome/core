3a140707183ac4559c2d5c828c19b3ef65e9a752
Doridian <git@doridian.net>
2025-11-03 21:55:02 -0800

    this computes serial

---
+++ nix/serial.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# TODO: Integrate with nix
+
+latest_commit_date="$(git show -s --format='%cs' HEAD)"
+first_commit_id_date="$(git log --after="$latest_commit_date 00:00" --before="$latest_commit_date 23:59" --pretty='format:%H' | tail -1)"
+
+date_commit_count="$(echo "00$(git rev-list --count "$first_commit_id_date..HEAD")" | grep -o '..$')"
+
+latest_calver="$(echo "$latest_commit_date" | sed 's/-//g')$date_commit_count"
+echo "$latest_calver"
