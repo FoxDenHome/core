6334eda9568d02ae1acd7caa9af088add46d64f4
Doridian <git@doridian.net>
2025-11-03 18:50:20 -0800

    fmt

--- terraform/domains/domain/main.tf
+++ terraform/domains/domain/main.tf
@@ -1,11 +1,3 @@
-module "ses" {
-  count     = var.ses ? 1 : 0
-  source    = "./ses"
-  zone      = cloudns_dns_zone.domain.id
-  domain    = var.domain
-  subdomain = ""
-}
-
 resource "cloudns_dns_zone" "domain" {
   domain = var.domain
   type   = "master"
@@ -16,3 +8,11 @@ function izone -a zone
   tofu import "module.domain[\"$zone\"].cloudns_dns_zone.domain" "$zone"
 end
 */
+
+module "ses" {
+  count     = var.ses ? 1 : 0
+  source    = "./ses"
+  zone      = cloudns_dns_zone.domain.id
+  domain    = var.domain
+  subdomain = ""
+}
