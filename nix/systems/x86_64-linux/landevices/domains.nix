1a567e5587c61ac15a0cd491ac090a068f362d25
Doridian <git@doridian.net>
2025-11-04 08:29:02 -0800

    fix

--- nix/systems/x86_64-linux/landevices/domains.nix
+++ nix/systems/x86_64-linux/landevices/domains.nix
@@ -49,6 +49,6 @@
     "doridian.de" = { admin = "hostmaster@doridian.de"; nameservers = ["ns1.doridian.de." "ns2.doridian.de." "ns3.doridian.de." "ns4.doridian.de."]; };
     "doridian.net" = { admin = "hostmaster@doridian.net"; nameservers = ["ns1.doridian.net." "ns2.doridian.net." "ns3.doridian.net." "ns4.doridian.net."]; };
     "foxden.network" = { admin = "hostmaster@foxden.network"; nameservers = ["ns1.foxden.network." "ns2.foxden.network." "ns3.foxden.network." "ns4.foxden.network."]; };
-    default = { admin = "support@cloudns.net"; nameservers = ["pns41.cloudns.net" "pns42.cloudns.net" "pns43.cloudns.net" "pns44.cloudns.net"]; };
+    default = { admin = "support@cloudns.net"; nameservers = ["pns41.cloudns.net." "pns42.cloudns.net." "pns43.cloudns.net." "pns44.cloudns.net."]; };
   };
 }
