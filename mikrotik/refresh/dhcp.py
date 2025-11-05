1f4fec7e2fd2b28a8270841d1a293b34ac14634b
Doridian <git@doridian.net>
2025-11-04 16:04:01 -0800

    Improve dhcp lease speed

--- mikrotik/refresh/dhcp.py
+++ mikrotik/refresh/dhcp.py
@@ -6,6 +6,9 @@ FILENAME = mtik_path("scripts/gen-dhcp.rsc")
 
 # TODO: Use API and diff in Python and then synchronize stuff instead of generating a big script (use comment as id)
 
+def print_lines(header: str, lines: list[str]) -> list[str]:
+    return [header, "set [find dynamic=no] comment=__REFRESHING__"] + sorted(lines) + ["remove [find comment=__REFRESHING__]"]
+
 def refresh_dhcp():
     unlink_safe("result")
     check_call(["nix", "build", f"{NIX_DIR}#dhcp.json.router"])
@@ -13,15 +16,6 @@ def refresh_dhcp():
         dhcp_leases = json_load(file)
     unlink_safe("result")
 
-    header_lines = [
-        "/ip/dhcp-server/lease/set [find dynamic=no] comment=__REFRESHING__",
-        "/ipv6/dhcp-server/binding/set [find dynamic=no] comment=__REFRESHING__",
-        ":local prev",
-    ]
-    trailer_lines = [
-        "/ip/dhcp-server/lease/remove [find comment=__REFRESHING__]",
-        "/ipv6/dhcp-server/binding/remove [find comment=__REFRESHING__]"
-    ]
     v4lines = []
     v6lines = []
     for lease in dhcp_leases:
@@ -48,7 +42,7 @@ def refresh_dhcp():
             v6lines.append(f':set prev [find {addr_attrib}]\n' + \
                         f':if ([:len $prev] > 0)' + \
                         f' do={{\n  set $prev {attribs}\n}}' + \
-                        f' else={{\n  remove [find ({addr_attrib} || {id_attrib})]\n  add {attribs}\n}}')
+                        f' else={{\n  remove [find (({addr_attrib}) || ({id_attrib}))]\n  add {attribs}\n}}')
 
     with open(FILENAME, "w") as file:
-        file.write(("\n".join(header_lines + ["/ip/dhcp-server/lease"] + sorted(v4lines) + ["/ipv6/dhcp-server/binding"] + sorted(v6lines) + trailer_lines)) + "\n")
+        file.write(("\n".join([":local prev"] + print_lines("/ip/dhcp-server/lease", v4lines) + print_lines("/ipv6/dhcp-server/binding", v6lines))) + "\n")
