d6de6e8eab689f52e80c4818182b2531e80b1595
Doridian <git@doridian.net>
2025-11-09 13:51:05 -0800

    fix modules

--- nix/systems/x86_64-linux/carvera-controller/base.nix
+++ nix/systems/x86_64-linux/carvera-controller/base.nix
@@ -10,7 +10,7 @@
 
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "nvme" "mpt3sas" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
