2401e9fff886921cb20cd3fa79d5f4b4e09ac736
Doridian <git@doridian.net>
2025-11-09 11:00:21 -0800

    Add router records

--- nix/systems/x86_64-linux/landevices/sshfp/make.sh
+++ nix/systems/x86_64-linux/landevices/sshfp/make.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+set -x
 cd "$(dirname "$0")"
-
-ssh-keyscan -D "$1" | grep -v '^;' | cut -d' ' -f4- > "records/$1"
+ssh-keyscan -D "$1" > "tmp-$1.txt"
+cat "tmp-$1.txt" | grep -v '^;' | cut -d' ' -f4- > "records/$1"
+rm -f "tmp-$1.txt"
