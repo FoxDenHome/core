7d847434c72c3be5b4794da4c116c1b68764fc4f
Doridian <git@doridian.net>
2025-11-09 15:05:30 -0800

    Use a writable, but ephemeral, home

--- nix/systems/x86_64-linux/carvera-controller/users.nix
+++ nix/systems/x86_64-linux/carvera-controller/users.nix
@@ -9,12 +9,31 @@
   users.users.appliance = {
     isSystemUser = true;
     group = "appliance";
-    home = ./appliance-home;
+    home = "/run/appliance";
     shell = pkgs.fish;
     linger = true;
     extraGroups = [ "seat" "video" "render" ];
   };
   users.groups.appliance = {};
+
+  systemd.tmpfiles.rules = [
+    "D /run/appliance 0700 appliance appliance"
+  ];
+
+  systemd.services.appliance-setup = {
+    serviceConfig = {
+      User = "appliance";
+      Group = "appliance";
+      WorkingDirectory = "/run/appliance";
+      ExecStart = [
+        "${pkgs.rsync}/bin/rsync -av --delete ${./appliance-home}/ /run/appliance/"
+        "${pkgs.coreutils}/bin/chmod 700 /run/appliance"
+        "${pkgs.coreutils}/bin/chown -R appliance:appliance /run/appliance"
+      ];
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
 
   environment.persistence."/nix/persist/appliance" = {
     hideMounts = true;
