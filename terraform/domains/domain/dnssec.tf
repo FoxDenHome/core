0afe1e37a47b1775b0535407b6a7d03b7438d617
Doridian <git@doridian.net>
2025-11-09 10:04:02 -0800

    Process DNSKEY with source

--- terraform/domains/domain/dnssec.tf
+++ terraform/domains/domain/dnssec.tf
@@ -6,7 +6,11 @@ data "external" "dnssec_json" {
 }
 
 locals {
-  dnskey_records = toset(jsondecode(data.external.dnssec_json.result.dnskeys))
+  dnskey_records = {for record in (jsondecode(data.external.dnssec_json.result.dnskeys) ):
+    split("|", record)[1] => {
+      source = split("|", record)[0]
+    }
+  }
 }
 
 output "dnskey_records" {
