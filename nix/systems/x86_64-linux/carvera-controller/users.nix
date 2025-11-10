aef1ab8b9e87a1340cdab730ba5a8d8f5dafd52e
Doridian <git@doridian.net>
2025-11-09 16:10:12 -0800

    Stop fish from complaining

--- nix/systems/x86_64-linux/carvera-controller/users.nix
+++ nix/systems/x86_64-linux/carvera-controller/users.nix
@@ -22,7 +22,7 @@
         "+${pkgs.coreutils}/bin/chown -R appliance:appliance /run/appliance"
         "+${pkgs.coreutils}/bin/chmod -R 700 /run/appliance"
         "${pkgs.rsync}/bin/rsync -av --delete ${./appliance-home}/ /run/appliance/"
-        "+${pkgs.coreutils}/bin/mkdir -p /run/appliance/tmp"
+        "${pkgs.coreutils}/bin/mkdir -p /run/appliance/tmp"
         "${pkgs.coreutils}/bin/chmod 700 /run/appliance/tmp"
         "${pkgs.coreutils}/bin/chmod 500 /run/appliance"
         "${pkgs.coreutils}/bin/mkdir -p /var/lib/appliance/data"
