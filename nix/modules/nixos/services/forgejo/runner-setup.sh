12327fb2dcdfa8c6f3e01fb23d8b52610f15ec44
Doridian <git@doridian.net>
2025-11-10 19:08:35 -0800

    try using /tmp

--- nix/modules/nixos/services/forgejo/runner-setup.sh
+++ nix/modules/nixos/services/forgejo/runner-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rundir="/run/user/$(id -u forgejo-runner)"
-mkdir -p "$rundir"
-chown forgejo-runner:forgejo-runner "$rundir"
-chmod 700 "$rundir"
+mkdir -p /tmp/run
+ln -s /tmp/run /run
+chown forgejo-runner:forgejo-runner /tmp/run
+chmod 700 /tmp/run
--- nix/modules/nixos/services/forgejo/runner.nix
+++ nix/modules/nixos/services/forgejo/runner.nix
@@ -68,9 +68,6 @@ in
             "${pkgs.coreutils}/bin/cp --update=all /registration.json /var/lib/forgejo-runner/.runner"
             "${pkgs.coreutils}/bin/chmod 600 /var/lib/forgejo-runner/.runner"
           ];
-          BindPaths = [
-            "/run/user-forgejo-runner:/run/user"
-          ];
           BindReadOnlyPaths = [
             "/run/wrappers/bin/newuidmap"
             "/run/wrappers/bin/newgidmap"
