a92a718d948e9cd6fc4f01fdfa708c64344f47e3
Doridian <git@doridian.net>
2025-11-10 19:11:38 -0800

    try using /tmp

--- nix/modules/nixos/services/forgejo/runner-setup.sh
+++ nix/modules/nixos/services/forgejo/runner-setup.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 rm -rf /tmp/run /run
 rundir="/run/user/$(id -u forgejo-runner)"
 mkdir -p "/tmp${rundir}"
-ln -s /run /tmp/run
+ln -s /tmp/run /run
 chown forgejo-runner:forgejo-runner "$rundir"
 chmod 700 "$rundir"
