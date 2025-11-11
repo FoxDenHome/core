6cfdde556a139a3abde565d6b546ec3b7a537725
Doridian <git@doridian.net>
2025-11-11 09:55:07 -0800

    dont exit

--- nix/modules/nixos/services/forgejo/runner.nix
+++ nix/modules/nixos/services/forgejo/runner.nix
@@ -117,8 +117,9 @@ in
             wants = [ "network.target" ];
 
             serviceConfig = {
+              Type = "exec";
               ExecStartPre = [ "+${./rundir-setup.sh}" ];
-              ExecStart = "${pkgs.podman}/bin/podman --log-level=info system service unix:///var/lib/forgejo-runner/podman.sock";
+              ExecStart = "${pkgs.podman}/bin/podman --log-level=info system service --time=0 unix:///var/lib/forgejo-runner/podman.sock";
               BindPaths = [
                 "/run/user-forgejo-runner-podman:/run/user"
               ];
