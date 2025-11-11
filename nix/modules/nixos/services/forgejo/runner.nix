631d88afe7fdd5e4ddc01810d93aaeff64bd4123
Doridian <git@doridian.net>
2025-11-10 18:35:15 -0800

    handle

--- nix/modules/nixos/services/forgejo/runner.nix
+++ nix/modules/nixos/services/forgejo/runner.nix
@@ -82,7 +82,7 @@ in
             "${./runner-config.yml}:/config.yml"
             "${config.sops.secrets."forgejo-runner-registration".path}:/registration.json"
           ];
-          ProtectProc = lib.mkForce "invisible";
+          PrivatePIDs = true;
           PrivateTmp = true;
           PrivateUsers = false;
           User = "forgejo-runner";
