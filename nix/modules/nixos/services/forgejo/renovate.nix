715b9d4f46a395cfd417deff69ea748d33bbbb32
Doridian <git@doridian.net>
2025-11-10 21:20:06 -0800

    And turn on renovate maybe

--- nix/modules/nixos/services/forgejo/renovate.nix
+++ nix/modules/nixos/services/forgejo/renovate.nix
@@ -61,7 +61,7 @@ in
         systemd.services.renovate = {
           confinement.packages = packages;
           serviceConfig = {
-            Restart = "on-failure";
+            Restart = "no";
             BindReadOnlyPaths = [
               # TODO: config.services.renovate.environment in 25.11
               config.systemd.services.renovate.environment.RENOVATE_CONFIG_FILE
