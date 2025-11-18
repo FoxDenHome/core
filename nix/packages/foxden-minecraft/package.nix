ebcdff26c2d6e5ea4e4f9b74fa35d95129fd9b33
Doridian <git@doridian.net>
2025-11-18 12:43:56 -0800

    Add http proxy for dynmap

--- nix/packages/foxden-minecraft/package.nix
+++ nix/packages/foxden-minecraft/package.nix
@@ -13,6 +13,7 @@ pkgs.stdenv.mkDerivation {
       url = "https://cdn.modrinth.com/data/E1XS8bXN/versions/rLfqDKHu/PlayerCollars-1.2.6%2B1.20.1-forge.jar";
       hash = "sha256-kEYZzR+tWaISRCkvZ0I1nHHXUabZxMdchs7YxX+HBqA=";
     })
+    # TODO: Managed dynmap/configuration.txt
     (pkgs.fetchurl {
       url = "https://mediafilez.forgecdn.net/files/4632/236/Dynmap-3.6-forge-1.20.jar";
       hash = "sha256-voYFsMvbiGlmAmbGCeveiRtsefaKUbljS3M51wjMkkg=";
