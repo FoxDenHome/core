9a3b794b29369136ce4443b5250ec7eec65d7c47
Doridian <git@doridian.net>
2025-11-18 22:51:32 -0800

    Add create bluemap pack

--- nix/packages/foxden-minecraft/package.nix
+++ nix/packages/foxden-minecraft/package.nix
@@ -29,7 +29,8 @@ pkgs.stdenv.mkDerivation {
       elif [ -d "$srcFile" ]; then
         rm -rf server-tmp && mkdir -p server-tmp
         cp -r "$srcFile"/* server-tmp
-        chmod 600 server-tmp/server-icon.png server-tmp/variables.txt server-tmp/server.properties server-tmp/*.json server-tmp/minecraft-*.sh server-tmp/nix-version.txt server-tmp/config || true
+        chmod 600 server-tmp/server-icon.png server-tmp/variables.txt server-tmp/server.properties server-tmp/*.json server-tmp/minecraft-*.sh server-tmp/nix-version.txt || true
+        chmod 700 server-tmp/config
         rm -fv server-tmp/server-icon.png server-tmp/variables.txt server-tmp/server.properties server-tmp/*.json server-tmp/minecraft-*.sh server-tmp/nix-version.txt
         cp -r server-tmp/* server/
       else
