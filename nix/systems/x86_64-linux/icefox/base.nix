b4ef152e9d06e2f148c7b49ce537149a3548e56b
Doridian <git@doridian.net>
2025-11-22 10:20:25 -0800

    Re-add routing

--- nix/systems/x86_64-linux/icefox/base.nix
+++ nix/systems/x86_64-linux/icefox/base.nix
@@ -4,7 +4,7 @@
   # Change them to "false" for first boot, before secrets exist
   # then, once stuff is done, set them to true
   foxDen.sops.available = true;
-  foxDen.boot.secure = true;
+  foxDen.boot.secure = false;
   foxDen.networking.provisionCriticalHosts = lib.mkForce false;
 
   system.stateVersion = "25.05";
