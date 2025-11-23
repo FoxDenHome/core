d586fa273e2b9a09dda6a24a83222c2741a222a9
Doridian <git@doridian.net>
2025-11-23 10:14:52 -0800

    Support GatewayOnLink

--- nix/modules/lib/hosts/main.nix
+++ nix/modules/lib/hosts/main.nix
@@ -164,6 +164,10 @@ in
               type = nullOr foxDenLib.types.ipWithoutCidr;
               default = null;
             };
+            GatewayOnLink = lib.mkOption {
+              type = bool;
+              default = false;
+            };
           };
         };
 
@@ -427,9 +431,9 @@ in
                       renderRoute = (
                         dev: route:
                         "${ipInNsCmd} route add "
-                        + (if route.Destination ? null != null then eSA route.Destination else "default")
-                        + (if route.Gateway ? null != null then " via ${eSA route.Gateway}" else " dev ${eSA dev}")
-                        + (if route.GatewayOnLink ? false == true then " onlink" else "")
+                        + (if route.Destination != null then eSA route.Destination else "default")
+                        + (if route.Gateway != null then " via ${eSA route.Gateway}" else " dev ${eSA dev}")
+                        + (if route.GatewayOnLink == true then " onlink" else "")
                       );
 
                       mkHooks = (
