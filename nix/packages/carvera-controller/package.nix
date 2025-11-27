356af6d1a35be3214f99da3744b49398378bf660
Doridian <git@doridian.net>
2025-11-27 10:44:07 -0800

    fmt

--- nix/packages/carvera-controller/package.nix
+++ nix/packages/carvera-controller/package.nix
@@ -2,12 +2,10 @@
 pkgs.stdenv.mkDerivation {
   name = "carvera-controller";
   version = "1.0.0";
-  src = (
-    pkgs.fetchzip {
-      url = "https://github.com/MakeraInc/CarveraController/releases/download/v0.9.11/carvera-controller-0.9.11-x86_64-linux.tar.xz";
-      hash = "sha256-YAROnzEaPIWbS3gdL1kr0Goj5+gAraxetZBIaOpqSnA=";
-    }
-  );
+  src = pkgs.fetchzip {
+    url = "https://github.com/MakeraInc/CarveraController/releases/download/v0.9.11/carvera-controller-0.9.11-x86_64-linux.tar.xz";
+    hash = "sha256-YAROnzEaPIWbS3gdL1kr0Goj5+gAraxetZBIaOpqSnA=";
+  };
 
   nativeBuildInputs = [
     pkgs.autoPatchelfHook
