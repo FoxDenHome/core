5264a74abeb3ad07d020676ecde57455ee02ef8e
Doridian <git@doridian.net>
2025-11-27 13:58:43 -0800

    Try making JRE better

--- nix/packages/foxden-minecraft/package.nix
+++ nix/packages/foxden-minecraft/package.nix
@@ -64,7 +64,8 @@ pkgs.stdenv.mkDerivation {
     cp -r ./aux/* "$out/server/"
     cp -nr ./modpack/* "$out/server/"
 
-    echo 'export "JAVA=${pkgs.corretto21}"' > "$out/minecraft-env.sh"
+    echo 'export "JAVA=${pkgs.corretto21}/bin/java"' > "$out/minecraft-env.sh"
+    echo 'export "PATH=$PATH:${pkgs.corretto21}/bin"' >> "$out/minecraft-env.sh"
 
     find "$out/server" -type d -exec chmod 500 {} +
   '';
