187b7699010b0b949f387baf5bd23fb8a5808ffc
Doridian <git@doridian.net>
2025-11-27 10:47:05 -0800

    update

--- terraform/servers/providers.tf
+++ terraform/servers/providers.tf
@@ -2,9 +2,13 @@ terraform {
   required_version = "~> 1.10"
 
   required_providers {
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.5"
+    }
     ovh = {
       source  = "ovh/ovh"
-      version = "~> 2.9.0"
+      version = "~> 2.10.0"
     }
   }
 
