5f49f1535d4298208be4d2ff7c842d48f8fdf57b
Doridian <git@doridian.net>
2025-11-27 10:29:50 -0800

    Improve build process

--- nix/packages/foxden-minecraft/package.nix
+++ nix/packages/foxden-minecraft/package.nix
@@ -2,13 +2,16 @@
 pkgs.stdenv.mkDerivation {
   name = "foxden-minecraft";
   version = "1.0.0";
-  srcs = [
-    (pkgs.fetchzip {
+
+  modpack = (
+    pkgs.fetchzip {
       url = "https://mediafilez.forgecdn.net/files/7179/411/All_of_Create_6.0_v2.1_serverpack.zip";
       name = "server";
       stripRoot = false;
       hash = "sha256-LEIJ891i8foqTMqEawHHv1cs2n6FrwySqotNirClsZg=";
-    })
+    }
+  );
+  mods = [
     (pkgs.fetchurl {
       url = "https://cdn.modrinth.com/data/E1XS8bXN/versions/rLfqDKHu/PlayerCollars-1.2.6%2B1.20.1-forge.jar";
       hash = "sha256-kEYZzR+tWaISRCkvZ0I1nHHXUabZxMdchs7YxX+HBqA=";
@@ -21,11 +24,12 @@ pkgs.stdenv.mkDerivation {
       url = "https://github.com/BlueMap-Minecraft/BlueMap/releases/download/v5.12/bluemap-5.12-mc1.20-6-forge.jar";
       hash = "sha256-J2Z9CdyUPsHncaIOLVk2ddCDUvH4d97xmeEyNoOPQ+0=";
     })
+  ];
+  bluemapPacks = [
     (pkgs.fetchurl {
       url = "https://github.com/Uiniel/BlueMapModelLoaders/releases/download/v0.3.2/BlueMapModelLoaders-0.3.2.jar";
       hash = "sha256-cavH3b0RcDocskO+/Ol/MxRhyPw4bp7O31IVDAi7q5U=";
     })
-    ./local
   ];
 
   buildInputs = with pkgs; [
@@ -34,35 +38,23 @@ pkgs.stdenv.mkDerivation {
   ];
 
   unpackPhase = ''
-    mkdir aux local modpack
-    for srcFile in $srcs; do
-      echo "Copying from $srcFile"
-      srcFileName="$(stripHash $srcFile)"
-      if [ "$srcFileName" == "local" ]; then
-        cp -r "$srcFile"/* local/
-        bash ${./build.sh} local aux
-      elif [ -d "$srcFile" ]; then
-        cp -r "$srcFile"/* modpack/
-      else
-        destDir="[INVALID]"
-        case "$srcFileName" in
-          *.sh|server-icon.png|server.properties)
-            destDir=""
-            ;;
-          BlueMapModelLoaders-*.jar|createentityaddon-*.jar)
-            destDir="config/bluemap/packs"
-            ;;
-          *.jar)
-            destDir="mods"
-            ;;
-          *)
-            echo "Unknown file type: $srcFile"
-            exit 1
-            ;;
-        esac
-        mkdir -p "aux/$destDir"
-        cp "$srcFile" "aux/$destDir/$srcFileName"
-      fi
+    mkdir aux
+    cp -r "$modpack" modpack/
+    cp -r "${./local}" local
+    bash ${./build.sh} local aux
+
+    copyaux() {
+      local srcFile="$1"
+      local destDir="$2"
+      mkdir -p "aux/$destDir"
+      cp "$srcFile" "aux/$destDir/$(stripHash "$srcFile")"
+    }
+
+    for pack in $bluemapPacks; do
+      copyaux "$pack" config/bluemap/packs
+    done
+    for mod in $mods; do
+      copyaux "$mod" mods
     done
 
     find aux local -type d -exec chmod 700 {} +
