02adb8b17e970482506c7f4d7c2e0956bbecbbdf
Doridian <git@doridian.net>
2025-11-28 11:45:54 -0800

    Add deploy job basics

--- nix/modules/nixos/maintenance.nix
+++ nix/modules/nixos/maintenance.nix
@@ -5,60 +5,51 @@
   ...
 }:
 let
-  updateScriptBase = ''
+  updateScript = ''
+    if [ -f /nix/persist/update.disable ]; then
+      echo "Auto-update disabled via /nix/persist/update.disable, skipping"
+      exit 1
+    fi
     set -xeuo pipefail
     nix flake update --flake 'git+https://git.foxden.network/FoxDen/core?dir=nix' || :
     nixos-rebuild switch --flake "git+https://git.foxden.network/FoxDen/core?dir=nix#$(hostname)" || :
-  '';
-
-  autoUpdateScript = pkgs.writeShellScript "nixos-auto-update.sh" ''
-    if [ -f /nix/persist/auto-update.disable ]; then
-      echo "Auto-update disabled via /nix/persist/auto-update.disable, skipping"
-      exit 1
-    fi
-    ${updateScriptBase}
     nix-collect-garbage --delete-older-than 30d
     /run/current-system/bin/switch-to-configuration boot
   '';
 
-  updateScript = pkgs.writeShellScript "nixos-update.sh" updateScriptBase;
-
-  pruneScript = pkgs.writeShellScript "nixos-prune.sh" ''
+  pruneAllScript = ''
     set -xeuo pipefail
     nix-collect-garbage --delete-old
     /run/current-system/bin/switch-to-configuration boot
   '';
 
-  cryptenrollScript = pkgs.writeShellScript "cryptenroll.sh" (
-    ''
-      #!/usr/bin/env bash
-      set -xeuo pipefail
-      enroll_disk() {
-        systemd-cryptenroll --wipe-slot tpm2 --tpm2-device auto --tpm2-pcrs '0:sha256+7:sha256+14:sha256' "$1"
-      }
-    ''
-    + (builtins.concatStringsSep "\n" (
-      map (dev: "enroll_disk ${dev.device}") (lib.attrsets.attrValues config.boot.initrd.luks.devices)
-    ))
-    + "\n"
-  );
+  cryptenrollScript = ''
+    #!/usr/bin/env bash
+    set -xeuo pipefail
+    enroll_disk() {
+      systemd-cryptenroll --wipe-slot tpm2 --tpm2-device auto --tpm2-pcrs '0:sha256+7:sha256+14:sha256' "$1"
+    }
+  ''
+  + (builtins.concatStringsSep "\n" (
+    map (dev: "enroll_disk ${dev.device}") (lib.attrsets.attrValues config.boot.initrd.luks.devices)
+  ))
+  + "\n";
 in
 {
-  environment.etc."foxden/nixos/update.sh".source = updateScript;
-  environment.etc."foxden/nixos/prune.sh".source = pruneScript;
-  environment.etc."foxden/cryptenroll.sh".source = cryptenrollScript;
+  environment.etc."foxden/cryptenroll.sh".source =
+    pkgs.writeShellScript "cryptenroll.sh" cryptenrollScript;
+  environment.etc."foxden/nixos-prune.sh".source =
+    pkgs.writeShellScript "nixos-prune-all.sh" pruneAllScript;
 
   systemd.services.foxden-auto-update = {
     description = "FoxDen NixOS auto-update service";
     after = [ "network.target" ];
+    conflicts = [ "foxden-auto-prune.service" ];
 
-    path = [
-      "/run/current-system/sw"
-    ];
-
+    path = [ "/run/current-system/sw" ];
+    script = updateScript;
     serviceConfig = {
       Type = "simple";
-      ExecStart = [ autoUpdateScript ];
       Restart = "no";
     };
   };
