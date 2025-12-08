84a1ff16422fc73c1e613054599366f200dc86e0
Doridian <git@doridian.net>
2025-12-08 12:16:27 -0800

    depend on stuff

--- nix/systems/x86_64-linux/bengalfox/zfs.nix
+++ nix/systems/x86_64-linux/bengalfox/zfs.nix
@@ -44,6 +44,11 @@ in
     ) zhddMounts
   );
 
+  systemd.services.zfs-import-zhdd = {
+    after = lib.mkForce [ "etc-zfs-zpool.cache.mount" "nix.mount" "systemd-modules-load.service" "systemd-ask-password-console.service" ];
+    wants = lib.mkForce [ "nix.mount" ];
+  };
+
   foxDen.zfs = {
     enable = true;
     sanoid = {
