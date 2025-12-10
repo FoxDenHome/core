94c7445aa58ae2176cd427513f4f6e19664a1df4
Doridian <git@doridian.net>
2025-12-09 16:57:19 -0800

    Sign PXE images

---
+++ mikrotik/files/tftp/sign.sh
@@ -0,0 +1,2 @@
+sbsign --key ~/Documents/foxden_pxe.key --cert ~/Documents/foxden_pxe.crt --output ipxe-arm64.efi ipxe-arm64.efi
+sbsign --key ~/Documents/foxden_pxe.key --cert ~/Documents/foxden_pxe.crt --output ipxe-x86_64.efi ipxe-x86_64.efi
