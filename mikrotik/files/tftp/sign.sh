89b495e4f59f22dc44f5cc7fd75fcc186d98faf2
Doridian <git@doridian.net>
2025-12-09 17:09:51 -0800

    Upload TFTP stuff auto

--- mikrotik/files/tftp/sign.sh
+++ mikrotik/files/tftp/sign.sh
@@ -1,2 +1,9 @@
-sbsign --key ~/Documents/foxden_pxe.key --cert ~/Documents/foxden_pxe.crt --output ipxe-arm64.efi ipxe-arm64.efi
-sbsign --key ~/Documents/foxden_pxe.key --cert ~/Documents/foxden_pxe.crt --output ipxe-x86_64.efi ipxe-x86_64.efi
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+sign() {
+	sbsign --key ~/Documents/foxden_pxe.key --cert ~/Documents/foxden_pxe.crt --output "$1" "$1"
+}
+for bin in *.efi; do
+	sign "$bin"
+done
