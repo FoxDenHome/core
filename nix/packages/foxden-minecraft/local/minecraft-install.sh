3343c873b7a067ddd2c4b43943b9f00964e4ca55
Doridian <git@doridian.net>
2025-12-12 16:20:39 -0800

    Write more files

--- nix/packages/foxden-minecraft/local/minecraft-install.sh
+++ nix/packages/foxden-minecraft/local/minecraft-install.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-LATEST_VERSION_FILE=/server/minecraft-modpack.id
-INSTALLED_VERSION_FILE="${SERVER_DIR}/minecraft-modpack.id"
+LATEST_ID_FILE=/server/minecraft-modpack.id
+INSTALLED_ID_FILE="${SERVER_DIR}/minecraft-modpack.id"
+
+LATEST_LOCK="$0"
+INSTALLED_LOCK_FILE="${SERVER_DIR}/minecraft-modpack.lock"
+
+superdel() {
+  chmod -R 700 "$@" || true
+  rm -rf "$@"
+}
 
 run_update() {
   cd "${SERVER_DIR}"
 
-  chmod -R 700 config/bluemap mods run.* libraries || true
-  rm -rf config/bluemap mods run.* libraries
+  superdel config/bluemap mods
+  INSTALLED_ID="$(cat "${INSTALLED_ID_FILE}" || echo none)"
+  LATEST_ID="$(cat "${LATEST_ID_FILE}")"
+  if [ "${INSTALLED_ID}" != "${LATEST_ID}" ]; then
+    echo "ID mismatch (current: ${INSTALLED_ID}, expected: ${LATEST_ID}), reinstalling"
+    superdel run.* libraries
+  fi
 
   find -type d -not -path './bluemap/*' -not -path './world/*' -exec chmod 700 {} \; || true
   find -type f -not -path './bluemap/*' -not -path './world/*' -exec chmod 600 {} \; || true
@@ -17,20 +30,21 @@ run_update() {
   find -type f -not -path './bluemap/*' -not -path './world/*' -exec chmod 600 {} \; || true
   chmod 700 ./*.sh
 
+  echo "${LATEST_LOCK}" > "${INSTALLED_LOCK_FILE}"
+
   exit 0
 }
 
-if [ ! -f "${INSTALLED_VERSION_FILE}" ]; then
-  echo "No version file found, assuming fresh install"
+if [ ! -f "${INSTALLED_LOCK_FILE}" ]; then
+  echo "No lock file found, assuming fresh install"
   run_update
 else
-  INSTALLED_VERSION="$(cat "${INSTALLED_VERSION_FILE}")"
-  LATEST_VERSION="$(cat "${LATEST_VERSION_FILE}")"
-  if [ "${INSTALLED_VERSION}" != "${LATEST_VERSION}" ]; then
-    echo "Version mismatch (current: ${INSTALLED_VERSION}, expected: ${LATEST_VERSION}), reinstalling"
+  INSTALLED_LOCK="$(cat "${INSTALLED_LOCK_FILE}")"
+  if [ "${INSTALLED_LOCK}" != "${LATEST_LOCK}" ]; then
+    echo "Lock mismatch (current: ${INSTALLED_LOCK}, expected: ${LATEST_LOCK}), upgrading"
     run_update
   else
-    echo "Version matches (${INSTALLED_VERSION}), no reinstall needed"
+    echo "Lock matches (${INSTALLED_LOCK}), no upgrade needed"
     exit 0
   fi
 fi
