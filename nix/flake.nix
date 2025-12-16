e84a93b86e3adc7e4f1eae1e84e6dc2467ad8168
Doridian <git@doridian.net>
2025-12-15 21:30:33 -0800

    Go stable

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,7 +7,7 @@
 
     impermanence.url = "github:nix-community/impermanence";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote";
+      url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
