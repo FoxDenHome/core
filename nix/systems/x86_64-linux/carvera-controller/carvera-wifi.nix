c7d68e76dd74f9494a485d9e89d32588e0923bc4
Doridian <git@doridian.net>
2025-12-24 23:25:12 -0800

    Add wifi 1

---
+++ nix/systems/x86_64-linux/carvera-controller/carvera-wifi.nix
@@ -0,0 +1,11 @@
+{ config, ... }:
+{
+  sops.secrets.wireless = config.lib.foxDen.sops.mkIfAvailable { };
+
+  networking.wireless = config.lib.foxDen.sops.mkIfAvailable {
+    secretsFile = config.sops.secrets.wireless.path;
+    networks = {
+      FoxDen_Carvera.pskRaw = "ext:psk_carvera";
+    };
+  };
+}
