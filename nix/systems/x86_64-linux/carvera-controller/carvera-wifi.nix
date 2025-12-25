4abd21803d2607e9c4b64d96889bb288d915dc4e
Doridian <git@doridian.net>
2025-12-24 23:27:58 -0800

    oh

--- nix/systems/x86_64-linux/carvera-controller/carvera-wifi.nix
+++ nix/systems/x86_64-linux/carvera-controller/carvera-wifi.nix
@@ -3,10 +3,12 @@
   sops.secrets.wireless = config.lib.foxDen.sops.mkIfAvailable { };
 
   networking.wireless = config.lib.foxDen.sops.mkIfAvailable {
+    enable = true;
     secretsFile = config.sops.secrets.wireless.path;
     networks = {
       FoxDen_Carvera.pskRaw = "ext:psk_carvera";
     };
+    extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
   };
 
   networking.networkmanager.enable = false;
