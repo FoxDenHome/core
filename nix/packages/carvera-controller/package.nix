8e3db098a8aa6e9422d37b4138b43b8e199bcd61
Doridian <git@doridian.net>
2025-12-24 23:42:47 -0800

    TODO, but this works

--- nix/packages/carvera-controller/package.nix
+++ nix/packages/carvera-controller/package.nix
@@ -94,6 +94,8 @@ let
 
     unpackPhase = "true";
 
+    # TODO: Figure out where this stores settings and make writable/persistent
+
     installPhase = ''
       cp -r $src $out
       chmod 755 $out $out/carveracontroller
