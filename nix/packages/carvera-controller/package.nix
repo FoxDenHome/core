5290f7132dee43fd1a4e067949f01583bd3ce35e
Doridian <git@doridian.net>
2025-12-24 17:25:12 -0800

    Fixup kivy almost

--- nix/packages/carvera-controller/package.nix
+++ nix/packages/carvera-controller/package.nix
@@ -4,25 +4,66 @@ let
 
   pypkgs-build-requirements = {
     altgraph = [ "setuptools" ];
+    buildozer = [ "setuptools" ];
     carvera-controller-community = [ "hatchling" ];
     certifi = [ "setuptools" ];
     charset-normalizer = [ "setuptools" ];
     distlib = [ "setuptools" ];
     docutils = [ "flit-core" ];
-    filelock = [ "hatchling" ];
+    filelock = [
+      "hatchling"
+      "hatch-vcs"
+    ];
     filetype = [ "setuptools" ];
     hid = [ "setuptools" ];
     idna = [ "flit-core" ];
     markupsafe = [ "setuptools" ];
     packaging = [ "flit-core" ];
-    platformdirs = [ "hatchling" ];
+    platformdirs = [
+      "hatchling"
+      "hatch-vcs"
+    ];
     pygments = [ "hatchling" ];
-    pyquicklz = [ "setuptools" ];
+    pyquicklz = [
+      "setuptools"
+      "cython"
+    ];
     pyserial = [ "setuptools" ];
     pyyaml = [ "setuptools" ];
     ruamel-yaml = [ "setuptools" ];
     ruamel-yaml-clib = [ "setuptools" ];
     toml = [ "setuptools" ];
+    ptyprocess = [ "flit-core" ];
+    jinja2 = [ "flit-core" ];
+    pexpect = [ "setuptools" ];
+    pyinstaller = [
+      "hatchling"
+      pkgs.zlib
+    ];
+    pyinstaller-versionfile = [ "poetry-core" ];
+    sh = [ "poetry-core" ];
+    typing-extensions = [ "flit-core" ];
+    urllib3 = [
+      "hatchling"
+      "hatch-vcs"
+    ];
+    virtualenv = [
+      "hatchling"
+      "hatch-vcs"
+    ];
+    requests = [ "setuptools" ];
+    kivy = [
+      "setuptools"
+      "cython"
+
+      pkgs.SDL2
+      pkgs.SDL2_image
+      pkgs.SDL2_ttf
+      pkgs.SDL2_mixer
+
+      pkgs.mesa
+      pkgs.gst_all_1.gstreamer
+    ];
   };
 in
 mkPoetryApplication {
@@ -38,6 +79,13 @@ mkPoetryApplication {
     builtins.mapAttrs (
       package: build-requirements:
       (builtins.getAttr package prev).overridePythonAttrs (old: {
+        configurePhase =
+          if package == "kivy" then
+            ''
+              export KIVY_NO_CONFIG=1
+            ''
+          else
+            "";
         buildInputs =
           (old.buildInputs or [ ])
           ++ (builtins.map (
