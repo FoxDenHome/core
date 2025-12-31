f49939cdeda63321c3edf0cfa43ef7bac5a967d9
Doridian <git@doridian.net>
2025-12-30 20:08:20 -0800

    a

---
+++ nix/modules/nixos/services/monitoring/ntfy-sh/main.nix
@@ -0,0 +1,59 @@
+{
+  foxDenLib,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  services = foxDenLib.services;
+  svcConfig = config.foxDen.services.ntfy-sh;
+  hostName = services.getFirstFQDN config svcConfig;
+
+  mkDir = (
+    dir: {
+      directory = dir;
+      user = config.services.ntfy-sh.user;
+      group = config.services.ntfy-sh.group;
+      mode = "u=rwx,g=,o=";
+    }
+  );
+in
+{
+  options.foxDen.services.ntfy-sh = (
+    services.http.mkOptions {
+      svcName = "ntfy-sh";
+      name = "Ntfy service";
+    }
+  );
+
+  config = lib.mkIf svcConfig.enable (
+    lib.mkMerge [
+      (services.make {
+        name = "ntfy-sh";
+        inherit svcConfig pkgs config;
+      }).config
+      (services.http.make {
+        inherit svcConfig pkgs config;
+        name = "http-ntfy-sh";
+        target = "proxy_pass http://127.0.0.1:2586;";
+      }).config
+      {
+        services.ntfy-sh.enable = true;
+        services.ntfy-sh.settings = {
+          base-url = "https://${hostName}";
+          listen-http = "127.0.0.1:2586";
+          behind-proxy = true;
+          auth-default-access = "deny-all";
+          enable-signup = false;
+        };
+        environment.persistence."/nix/persist/ntfy-sh" = {
+          hideMounts = true;
+          directories = [
+            (mkDir "/var/lib/ntfy-sh")
+          ];
+        };
+      }
+    ]
+  );
+}
