9cc360eb15324e729ab0e8d1768b63dbac7bca96
Doridian <git@doridian.net>
2025-12-31 17:37:57 -0800

    Add DNS records for mail proxy

--- nix/systems/x86_64-linux/icefox/mailrelay.nix
+++ nix/systems/x86_64-linux/icefox/mailrelay.nix
@@ -1,6 +1,12 @@
-{ foxDenLib, config, ... }:
+{
+  foxDenLib,
+  nixpkgs,
+  config,
+  ...
+}:
 let
   util = foxDenLib.util;
+  mkDirectives = ips: map (ip: if util.isIPv6 ip then "ip6:${ip}" else "ip4:${ip}") ips;
 in
 {
   config.foxDen.dns.records = [
@@ -8,8 +14,19 @@ in
       fqdn = "hosts.foxden.network";
       type = "TXT";
       ttl = 3600;
-      value = "v=spf1 ${map (ip: if util.isIPv6 ip then "ip6:${ip}" else "ip4:${ip}") config.lib.foxDenSys.mainIPs} -all";
-      horizon = "*";
+      value = "v=spf1 ${nixpkgs.lib.concatStringsSep " " (mkDirectives config.lib.foxDenSys.mainIPs)} -all";
+      horizon = "internal";
+    }
+    {
+      fqdn = "hosts.foxden.network";
+      type = "TXT";
+      ttl = 3600;
+      value = "v=spf1 ${
+        nixpkgs.lib.concatStringsSep " " (
+          mkDirectives (nixpkgs.lib.filter (ip: !util.isPrivateIP ip) config.lib.foxDenSys.mainIPs)
+        )
+      } -all";
+      horizon = "external";
     }
   ];
 }
