015198f41d7822cc9d95ab94c2991399d9ca8d8e
Doridian <git@doridian.net>
2026-01-14 20:02:07 -0800

    Late-merge the startup.txt to make compiles faster

--- nix/packages/circuitjs/package.nix
+++ nix/packages/circuitjs/package.nix
@@ -1,64 +1,80 @@
 { pkgs, ... }:
 let
-  gradle = pkgs.gradleFromWrapper {
-    wrapperPropertiesPath = ./gradle-wrapper.properties;
-    defaultJava = pkgs.openjdk8;
-  };
   version = "1.0.0";
 
-  buildGradleApplication = (
-    pkgs.buildGradleApplication.override {
-      stdenvNoCC = pkgs.stdenvNoCC // {
-        mkDerivation = (
-          input:
-          pkgs.stdenvNoCC.mkDerivation (
-            input
-            // {
-              installPhase = ''
-                mkdir -p $out/share
-                mv war $out/share/circuitjs
-                mv build/gwt/out/circuitjs1 $out/share/circuitjs/
-                cp ${./startup.txt} $out/share/circuitjs/circuitjs1/circuits/startup.txt
-              '';
-              nativeBuildInputs = [ gradle ];
-              postFixup = "true";
-            }
-          )
-        );
+  javaPkg =
+    let
+      gradle = pkgs.gradleFromWrapper {
+        wrapperPropertiesPath = ./gradle-wrapper.properties;
+        defaultJava = pkgs.openjdk8;
       };
-    }
-  );
-in
-buildGradleApplication {
-  pname = "circuitjs";
-  inherit gradle version;
 
-  buildTask = "compileGwt";
+      buildGradleApplication = (
+        pkgs.buildGradleApplication.override {
+          stdenvNoCC = pkgs.stdenvNoCC // {
+            mkDerivation = (
+              input:
+              pkgs.stdenvNoCC.mkDerivation (
+                input
+                // {
+                  installPhase = ''
+                    mv war $out
+                    mv build/gwt/out/circuitjs1 $out/
+                  '';
+                  nativeBuildInputs = [ gradle ];
+                  postFixup = "true";
+                }
+              )
+            );
+          };
+        }
+      );
+    in
+    buildGradleApplication {
+      pname = "circuitjs-java";
+      inherit gradle version;
 
-  src = pkgs.stdenvNoCC.mkDerivation {
-    name = "circuitjs-src";
-    inherit version;
+      buildTask = "compileGwt";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "johnnewto";
-      repo = "circuitjs1";
-      rev = "10a9037254711e67363a0f35d4daf32f28d3bed3";
-      sha256 = "sha256-Z/gJeVbVQEo/LYxDJFSX1hsMOdOTYeg0i2usZx24LLY=";
+      src = pkgs.stdenvNoCC.mkDerivation {
+        name = "circuitjs-src";
+        inherit version;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "johnnewto";
+          repo = "circuitjs1";
+          rev = "10a9037254711e67363a0f35d4daf32f28d3bed3";
+          sha256 = "sha256-Z/gJeVbVQEo/LYxDJFSX1hsMOdOTYeg0i2usZx24LLY=";
+        };
+
+        unpackPhase = "true";
+        # ./gradlew --refresh-dependencies --write-verification-metadata sha256 --write-locks dependencies
+        installPhase = ''
+          cp -r $src $out
+
+          chmod -R 755 $out
+          mkdir -p $out/gradle
+          chmod 755 $out/gradle
+
+          cp ${./settings.gradle} $out/settings.gradle
+          cp ${./verification-metadata.xml} $out/gradle/verification-metadata.xml
+
+          cd $out && patch -p1 -i ${./circuitjs.patch}
+        '';
+      };
     };
+in
+pkgs.stdenvNoCC.mkDerivation {
+  pname = "circuitjs";
+  inherit version;
 
-    unpackPhase = "true";
-    # ./gradlew --refresh-dependencies --write-verification-metadata sha256 --write-locks dependencies
-    installPhase = ''
-      cp -r $src $out
+  src = javaPkg;
 
-      chmod -R 755 $out
-      mkdir -p $out/gradle
-      chmod 755 $out/gradle
-
-      cp ${./settings.gradle} $out/settings.gradle
-      cp ${./verification-metadata.xml} $out/gradle/verification-metadata.xml
-
-      cd $out && patch -p1 -i ${./circuitjs.patch}
-    '';
-  };
+  unpackPhase = "true";
+  installPhase = ''
+    mkdir -p $out/share
+    cp -r $src $out/share/circuitjs
+    chmod 755 $out/share/circuitjs/circuitjs1/circuits
+    cp ${./startup.txt} $out/share/circuitjs/circuitjs1/circuits/startup.txt
+  '';
 }
