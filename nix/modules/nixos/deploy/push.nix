f7acf828dbb1ed2c476de849f9f39e4e13b9ceb5
Doridian <git@doridian.net>
2026-01-25 19:39:05 -0800

    Try adding push

--- nix/modules/nixos/deploy/push.nix
+++ nix/modules/nixos/deploy/push.nix
@@ -1,9 +1,16 @@
 {
   config,
   pkgs,
+  hostName,
   lib,
   ...
 }:
+let
+  pusherSubnets = [
+    "10.6.11.0/24"
+    "fd2c:f4cb:63be:6::b00/120"
+  ];
+in
 {
   options.foxDen.deploy.push = lib.mkEnableOption "Enable push updates";
   config = lib.mkIf config.foxDen.deploy.push {
@@ -22,5 +29,18 @@
       ];
     };
     users.groups.nixpush = { };
+
+    foxDen.firewall.rules = map (subnet: {
+      table = "filter";
+      chain = "forward";
+      action = "accept";
+      source = subnet;
+      destination = {
+        host = hostName;
+      };
+      dstport = 22;
+      protocol = "tcp";
+      comment = "nixpush-allow-ssh";
+    }) pusherSubnets;
   };
 }
