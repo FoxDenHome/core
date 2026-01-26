ba7e98ffa56549a31bc789b6b4aad18f87258eba
Doridian <git@doridian.net>
2026-01-25 19:05:37 -0800

    Add push deploy user

--- nix/modules/nixos/deploy/push.nix
+++ nix/modules/nixos/deploy/push.nix
@@ -1,7 +1,23 @@
-{ config, lib, ... } :
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   options.foxDen.deploy.push = lib.mkEnableOption "Enable push updates";
   config = lib.mkIf config.foxDen.deploy.push {
-    
+    users.users.nixpush = {
+      isNormalUser = true;
+      autoSubUidGidRange = false;
+      home = "/home/nixpush";
+      shell = pkgs.fish;
+      group = "nixpush";
+      extraGroups = [ "maintainers" ];
+      openssh.authorizedKeys.keys = [
+        "command=\"/etc/foxden/nixos/update.sh\" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPe7TWvlE5VP9fn5YBHE0qsYZpD4Ev0He2aTUWHJBo2y nixpush"
+      ];
+    };
+    users.groups.nixpush = { };
   };
 }
