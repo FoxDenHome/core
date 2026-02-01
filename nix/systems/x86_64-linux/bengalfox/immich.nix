ca5427d21e0cfb341e75af535a73ec6f76217ece
Doridian <git@doridian.net>
2026-01-31 21:10:51 -0800

    Begin moving immich back

---
+++ nix/systems/x86_64-linux/bengalfox/immich.nix
@@ -0,0 +1,33 @@
+{ config, ... }:
+let
+  mkVlanHost = config.lib.foxDenSys.mkVlanHost;
+in
+{
+  foxDen.services = config.lib.foxDen.sops.mkIfAvailable {
+    immich = {
+      enable = true;
+      host = "immich";
+      tls.enable = true;
+      oAuth = {
+        enable = true;
+        clientId = "immich";
+        displayName = "Immich";
+        imageFile = ../../../files/icons/immich.svg;
+      };
+    };
+  };
+
+  foxDen.hosts.hosts = {
+    immich = mkVlanHost 2 {
+      dns = {
+        fqdns = [ "images.foxden.network" ];
+        dynDns = true;
+      };
+      webservice.enable = true;
+      addresses = [
+        "10.2.11.23/16"
+        "fd2c:f4cb:63be:2::b17/64"
+      ];
+    };
+  };
+}
