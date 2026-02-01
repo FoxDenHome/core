13999837591f01a3116e206371a853f69a1b4553
Doridian <git@doridian.net>
2026-01-31 19:03:02 -0800

    dense

--- nix/modules/nixos/services/immich.nix
+++ nix/modules/nixos/services/immich.nix
@@ -12,7 +12,7 @@ let
   hostName = services.getFirstFQDN config svcConfig;
   proto = if svcConfig.tls.enable then "https" else "http";
 
-  clipModelName = "ViT-L-14-quickgelu";
+  clipModelName = "ViT-B-16-SigLIKP-512__webli";
   facialRecognitionModelName = "antelopev2";
 in
 {
