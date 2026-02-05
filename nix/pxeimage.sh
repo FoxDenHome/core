4dd0f5d8e403cf7b8ffd9d1fbfcd870723cfca8a
Doridian <git@doridian.net>
2026-02-05 15:45:24 -0800

    stuff

---
+++ nix/pxeimage.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+OUTDIR="$1"
+MACHINE="${2-amd64-netboot}"
+
+scriptdir="$(dirname "$(realpath "$0")")"
+cd "$scriptdir"
+
+buildsub() {
+    nix build ".#nixosConfigurations.$MACHINE.config.system.build.$1" --no-link --print-out-paths
+}
+
+cp "$(buildsub netbootIpxeScript)/netboot.ipxe" "${OUTDIR}/netboot.ipxe"
+cp "$(buildsub netbootRamdisk)/initrd" "${OUTDIR}/initrd"
+cp "$(buildsub uki)/nixos.efi" "${OUTDIR}/bzImage"
