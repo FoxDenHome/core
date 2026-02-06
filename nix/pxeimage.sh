5a32dbb7b14d112f5286dfa285ab4204842c3592
Doridian <git@doridian.net>
2026-02-05 17:10:20 -0800

    We can now actually build a netboot UKI

--- nix/pxeimage.sh
+++ nix/pxeimage.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-arch="${2-x86_64}"
 OUTDIR="$1"
-MACHINE="${arch}-netboot"
 
-scriptdir="$(dirname "$(realpath "$0")")"
-cd "$scriptdir"
-
-buildsub() {
-    nix build ".#nixosConfigurations.$MACHINE.config.system.build.$1" --no-link --print-out-paths
-}
+cd "$(dirname "$(realpath "$0")")"
 
 copy_out() {
     cp "$1" "${OUTDIR}/$2"
@@ -22,4 +15,9 @@ copy_out_signed() {
     sbsign --key ~/Documents/foxden_pxe.key --cert ~/Documents/foxden_pxe.crt --output "${OUTDIR}/$2" "${OUTDIR}/$2"
 }
 
-copy_out_signed "$(buildsub uki)/nixos.efi" "uki-${arch}.efi"
+build_arch() {
+    copy_out_signed "$(nix build ".#nixosConfigurations.$1-netboot.config.system.build.uki" --no-link --print-out-paths)/nixos.efi" "uki-$1.efi"
+}
+
+build_arch x86_64
+#build_arch aarch64
