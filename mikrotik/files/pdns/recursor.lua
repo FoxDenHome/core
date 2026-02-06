9108e804f20e5bf5a1add69f9ec5609efe7c561f
Doridian <git@doridian.net>
2026-02-06 14:22:37 -0800

    localize

--- mikrotik/files/pdns/recursor.lua
+++ mikrotik/files/pdns/recursor.lua
@@ -1,16 +1,19 @@
+local qtype_AAAA = pdns.AAAA
+local policy_NODATA = pdns.policykinds.NODATA
+
 local ipv6_block_set = newDS()
 ipv6_block_set:add{
     "google.com"
 }
 
 function preresolve(dq)
-    if dq.qtype ~= pdns.AAAA then
+    if dq.qtype ~= qtype_AAAA then
         return false
     end
     if not ipv6_block_set:check(dq.qname) then
         return false
     end
 
-    dq.appliedPolicy.policyKind = pdns.policykinds.NODATA
+    dq.appliedPolicy.policyKind = policy_NODATA
     return false
 end
