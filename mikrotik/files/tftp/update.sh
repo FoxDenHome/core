00074ac600e674e3589c5b154bf2525e229d42ed
Doridian <git@doridian.net>
2026-02-05 16:47:28 -0800

    update

--- mikrotik/files/tftp/update.sh
+++ mikrotik/files/tftp/update.sh
@@ -17,12 +17,10 @@ copy_and_sign() {
 copy_arch() {
 	copy_and_sign "$ipxedir/$1/ipxe.efi" "ipxe-$1.efi"
 	copy_and_sign "$ipxedir/$1/snp.efi" "ipxe-$1-snponly.efi"
+	if [ -f "$ipxedir/$1/wimboot.efi" ]; then
+		copy_and_sign "$ipxedir/$1/wimboot.efi" "wimboot-$1.efi"
+	fi
 }
 
-copy_arch_wimboot() {
-	copy_arch "$1"
-	copy_and_sign "$ipxedir/$1/wimboot.efi" "wimboot-$1.efi"
-}
-
-copy_arch_wimboot x86_64
+copy_arch x86_64
 copy_arch aarch64
