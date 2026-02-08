3d2c0864f8a47aeee6ab90dc0adcc378992e0fc3
Doridian <git@doridian.net>
2026-02-08 12:00:02 -0800

    New RAID

--- nix/systems/x86_64-linux/islandfox/base.nix
+++ nix/systems/x86_64-linux/islandfox/base.nix
@@ -35,7 +35,7 @@
     enable = true;
     mdadmConf = ''
       MAILADDR mdmon@doridian.net
-      ARRAY /dev/md0 metadata=1.2 UUID=136007e2:4e9703e7:7ea3b9c8:fd1b6cff
+      ARRAY /dev/md0 metadata=1.2 UUID=44bb6487:5f9e160b:901236f6:191fb9b4
     '';
   };
 
