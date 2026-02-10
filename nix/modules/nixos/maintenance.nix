2a79a4bd9c0e78bc3ac479c37a06a4da19a9a7f1
Doridian <git@doridian.net>
2026-02-10 12:55:29 -0800

    Allow manually enrolling a disk

--- nix/modules/nixos/maintenance.nix
+++ nix/modules/nixos/maintenance.nix
@@ -22,6 +22,12 @@ let
     enroll_disk() {
       systemd-cryptenroll --wipe-slot tpm2 --tpm2-device auto --tpm2-pcrs '0:sha256+7:sha256+14:sha256' "$1"
     }
+
+    manual_disk="$\{1-\}"
+    if [ ! -z "$manual_disk" ]; then
+      enroll_disk "$manual_disk"
+      exit 0
+    fi
   ''
   + (builtins.concatStringsSep "\n" (
     map (dev: "enroll_disk ${dev.device}") (lib.attrsets.attrValues config.boot.initrd.luks.devices)
