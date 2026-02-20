d1395288e2482c8aa1bf8ee6d9f18c13e7fa168a
Doridian <git@doridian.net>
2026-02-19 21:37:52 -0800

    Add experiments to core

--- nix/modules/nixos/boot-uki.nix
+++ nix/modules/nixos/boot-uki.nix
@@ -11,19 +11,15 @@ let
 
   ini = pkgs.formats.ini { };
 
-  ukiCfg =
-    profile:
-    ini.generate "ukify.conf" {
-      UKI = {
-        Linux = "${profile}/kernel";
-        Initrd = "${profile}/initrd";
-        Cmdline = "__CMDLINE__";
-        Stub = "${pkgs.systemd}/lib/systemd/boot/efi/linux${efiArch}.efi.stub";
-        OSRelease = "@${config.system.build.etc}/etc/os-release";
-      };
+  ukiCfg = ini.generate "ukify.conf" {
+    UKI = {
+      Linux = "__PROFILE__/kernel";
+      Initrd = "__PROFILE__/initrd";
+      Cmdline = "__CMDLINE__";
+      Stub = "${pkgs.systemd}/lib/systemd/boot/efi/linux${efiArch}.efi.stub";
+      OSRelease = "@${config.system.build.etc}/etc/os-release";
     };
-
-  profileDir = "/nix/var/nix/profiles/system";
+  }; # boot${efiArch}.efi
 in
 {
   options.foxDen.boot.uki = lib.mkEnableOption "Enable direct UKI boot";
@@ -37,31 +33,65 @@ in
           set -euo pipefail
           export PATH="$PATH:${pkgs.coreutils}/bin"
 
-          TEMPDIR="$( mktemp -d)"
-          cp "${ukiCfg profileDir}" "$TEMPDIR/ukify.conf"
-          echo "init=$(cat ${profileDir}/boot.json | ${pkgs.jq}/bin/jq -r '."org.nixos.bootspec.v1".init') $(cat ${profileDir}/kernel-params)" > "$TEMPDIR/cmdline"
-          ${pkgs.gnused}/bin/sed -i "s|__CMDLINE__|@$TEMPDIR/cmdline|" "$TEMPDIR/ukify.conf"
-          ${pkgs.buildPackages.systemdUkify}/lib/systemd/ukify build \
-            --config="$TEMPDIR/ukify.conf" \
-            --output="$TEMPDIR/boot${efiArch}.efi"
+          TEMPDIR="$(mktemp -d)"
+          FIXED_PROFILES=/nix/var/nix/profiles/system-*-link
+          MAIN_PROFILE=/nix/var/nix/profiles/system
+
+          makeuki() {
+            local name="$1"
+            local profile="$2"
+            if [ -f "$TEMPDIR/$1.efi" ]; then
+              return 0
+            fi
+            cp -f "${ukiCfg}" "$TEMPDIR/ukify.conf"
+            echo "init=$(cat "$profile/boot.json" | ${pkgs.jq}/bin/jq -r '."org.nixos.bootspec.v1".init') $(cat "$profile/kernel-params")" > "$TEMPDIR/cmdline"
+            ${pkgs.gnused}/bin/sed -i "s|__CMDLINE__|@$TEMPDIR/cmdline|" "$TEMPDIR/ukify.conf"
+            ${pkgs.gnused}/bin/sed -i "s|__PROFILE__|$profile|" "$TEMPDIR/ukify.conf"
+            ${pkgs.buildPackages.systemdUkify}/lib/systemd/ukify build \
+              --config="$TEMPDIR/ukify.conf" \
+              --output="$TEMPDIR/$name.efi"
         ''
-        + (
-          if config.foxDen.boot.secure then
+        +
+          (
+            if config.foxDen.boot.secure then
+              ''
+                ${pkgs.sbsigntool}/bin/sbsign \
+                            --key /etc/secureboot/keys/db/db.key \
+                            --cert /etc/secureboot/keys/db/db.pem \
+                            --output "$TEMPDIR/$name.efi" \
+                            "$TEMPDIR/$name.efi"
+              ''
+            else
+              "# SecureBoot is off"
+          )
             ''
-              ${pkgs.sbsigntool}/bin/sbsign \
-                          --key /etc/secureboot/keys/db/db.key \
-                          --cert /etc/secureboot/keys/db/db.pem \
-                          --output "$TEMPDIR/boot${efiArch}.efi" \
-                          "$TEMPDIR/boot${efiArch}.efi"
+              }
+
+              copyuki() {
+                local esp="$1"
+                shift 1
+                local name="$1"
+                makeuki "$@"
+                cp -f "$TEMPDIR/$name.efi" "$esp/$name.efi"
+              }
+
+              buildesp() {
+                local esp="$1/EFI/TEST"
+                for profile in $FIXED_PROFILES; do
+                  local name="nixos-$(basename "$profile")"
+                  if [ -f "$esp/$name.efi" ]; then
+                    continue
+                  fi
+                  copyuki "$esp" "$name"
+                done
+                makeuki "boot${efiArch}" "$MAIN_PROFILE"
+                copyuki "$esp" "$name"
+              }
             ''
-          else
-            "# SecureBoot is off"
-        )
         + (lib.concatStringsSep "\n" (
           map (
             esp:
             let
-              # TODO: We crash on finding the NixOS closure, so likely wrong initrd or cmdline
               espDir = "${esp}/EFI/TEST";
             in
             ''
