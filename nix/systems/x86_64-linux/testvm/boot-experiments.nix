bbabb51b910f54768d456d5adbe3abf0dcebe3ce
Doridian <git@doridian.net>
2026-02-19 17:40:59 -0800

    try this

--- nix/systems/x86_64-linux/testvm/boot-experiments.nix
+++ nix/systems/x86_64-linux/testvm/boot-experiments.nix
@@ -1,4 +1,15 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
+let
+  uki = "${config.system.build.uki}/nixos.efi";
+  espMounts = [
+    "/boot"
+  ];
+in
 {
-  # TODO: All of this
+    system.activationScripts.installFoxDenUki = {
+      text = lib.concatStringsSep "\n" (map (esp: ''
+        ${pkgs.coreutils}/bin/mkdir -p ${esp}/EFI_/BOOT
+        ${pkgs.coreutils}/bin/cp ${uki} ${esp}/EFI_/BOOT/BOOTX64.EFI
+      '') espMounts);
+    };
 }
