eb87a5abcfb8f17e99543aed93cae24bffd2ada6
Doridian <git@doridian.net>
2026-02-19 17:42:46 -0800

    start

--- nix/systems/x86_64-linux/testvm/boot-experiments.nix
+++ nix/systems/x86_64-linux/testvm/boot-experiments.nix
@@ -6,10 +6,10 @@ let
   ];
 in
 {
-    system.activationScripts.installFoxDenUki = {
-      text = lib.concatStringsSep "\n" (map (esp: ''
-        ${pkgs.coreutils}/bin/mkdir -p ${esp}/EFI_/BOOT
-        ${pkgs.coreutils}/bin/cp ${uki} ${esp}/EFI_/BOOT/BOOTX64.EFI
-      '') espMounts);
-    };
+    # system.activationScripts.installFoxDenUki = {
+    #   text = lib.concatStringsSep "\n" (map (esp: ''
+    #     ${pkgs.coreutils}/bin/mkdir -p ${esp}/EFI_/BOOT
+    #     ${pkgs.coreutils}/bin/cp ${uki} ${esp}/EFI_/BOOT/BOOTX64.EFI
+    #   '') espMounts);
+    # };
 }
