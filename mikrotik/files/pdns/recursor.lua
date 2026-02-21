48cf59cf0e4dca4ccd48c60e14e92a7146084cd5
Doridian <git@doridian.net>
2026-02-21 02:01:03 -0800

    Revert "allow v6"
    This reverts commit 75e94f5a48c0b866c596f2c1b0abe4c304d3ca14.

---
+++ mikrotik/files/pdns/recursor.lua
@@ -0,0 +1,16 @@
+local qtype_AAAA = pdns.AAAA
+local policy_NODATA = pdns.policykinds.NODATA
+
+local ipv6_block_set = newDS()
+--ipv6_block_set:add{
+--    "google.com"
+--}
+
+function preresolve(dq)
+    if dq.qtype ~= qtype_AAAA or not ipv6_block_set:check(dq.qname) then
+        return false
+    end
+
+    dq.appliedPolicy.policyKind = policy_NODATA
+    return false
+end
