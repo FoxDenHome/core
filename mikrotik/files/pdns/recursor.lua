419457568b4bcbfd879e626f8077608ea904498a
Doridian <git@doridian.net>
2026-02-21 02:01:49 -0800

    ipv4onlt

--- mikrotik/files/pdns/recursor.lua
+++ mikrotik/files/pdns/recursor.lua
@@ -2,9 +2,9 @@ local qtype_AAAA = pdns.AAAA
 local policy_NODATA = pdns.policykinds.NODATA
 
 local ipv6_block_set = newDS()
---ipv6_block_set:add{
---    "google.com"
---}
+ipv6_block_set:add{
+    "furaffinity.net"
+}
 
 function preresolve(dq)
     if dq.qtype ~= qtype_AAAA or not ipv6_block_set:check(dq.qname) then
