37a5ad85287711516ebdc9c9c7e939643eb3d61a
Doridian <git@doridian.net>
2026-03-12 20:29:04 -0700

    Updated broadcast-box

---
+++ nix/packages/broadcast-box/package.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  name = "broadcast-box";
+  version = "2.0.2";
+
+  src = pkgs.fetchFromGitHub {
+    repo = "broadcast-box";
+    owner = "Glimesh";
+    rev = "v${version}";
+    hash = "sha256-5Sk2dKIiwthZv9/cI+++ix0VAcxpSsoj8jsHZ3G+pHQ=";
+  };
+
+  frontend = pkgs.buildNpmPackage {
+    inherit version;
+    pname = "${name}-web";
+    src = "${src}/web";
+
+    npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
+    npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+
+    preBuild = ''
+      # The VITE_API_PATH environment variable is needed
+      cp "${src}/.env.production" ../
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp -r build $out
+    '';
+  };
+in
+pkgs.buildGoModule {
+  inherit version src frontend;
+  pname = name;
+  vendorHash = "sha256-3/ZuNHU1MJD5ew7abWU81EF/G2fJ6eQLslSEnv7m+Xg=";
+  proxyVendor = true; # fixes darwin/linux hash mismatch
+
+  postPatch = ''
+    substituteInPlace internal/environment/environment.go \
+      --replace-fail './web/build' '${placeholder "out"}/share'
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share
+    cp -r $frontend/build/* $out/share
+
+    install -Dm755 $GOPATH/bin/broadcast-box -t $out/bin
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "WebRTC broadcast server";
+    homepage = "https://github.com/Glimesh/broadcast-box";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ JManch ];
+    platforms = lib.platforms.unix;
+    mainProgram = "broadcast-box";
+  };
+}
