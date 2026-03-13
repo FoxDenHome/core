eff0f6ed2b0161db9002a5b67189d4b1cb622dc7
Doridian <git@doridian.net>
2026-03-12 19:34:51 -0700

    Try adding bcat-box

---
+++ nix/modules/nixos/services/broadcast-box.nix
@@ -0,0 +1,53 @@
+{
+  foxDenLib,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  services = foxDenLib.services;
+
+  svcConfig = config.foxDen.services.broadcast-box;
+in
+{
+  options.foxDen.services.broadcast-box = (
+    services.http.mkOptions {
+      svcName = "broadcast-box";
+      name = "Broadcast Box";
+    }
+  );
+
+  config = lib.mkIf svcConfig.enable (
+    lib.mkMerge [
+      (services.make {
+        name = "broadcast-box";
+        gpu = true;
+        inherit svcConfig pkgs config;
+      }).config
+      (services.http.make {
+        inherit svcConfig pkgs config;
+        name = "http-broadcast-box";
+        target = "proxy_pass http://127.0.0.1:${config.services.broadcast-box.web.port};";
+      }).config
+      {
+        sops.secrets.broadcast-box = config.lib.foxDen.sops.mkIfAvailable { };
+        services.broadcast-box = {
+          enable = true;
+          settings = {
+            STREAM_PROFILE_POLICY = "RESERVED";
+            INCLUDE_PUBLIC_IP_IN_NAT_1_TO_1_IP = true;
+            STREAM_PROFILE_PATH = "/var/lib/broadcast-box/stream-profiles";
+          };
+        };
+        systemd.services.broadcast-box = {
+          serviceConfig = {
+            EnvironmentFile = config.lib.foxDen.sops.mkIfAvailable config.sops.secrets.broadcast-box.path;
+            StateDirectory = "broadcast-box";
+            DynamicUser = true; # already set upstream, but we MUST fail if that changes, because only /var/lib/private is in impermanence by default!
+          };
+        };
+      }
+    ]
+  );
+}
