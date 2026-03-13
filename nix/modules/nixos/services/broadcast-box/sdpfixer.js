36aad9e9fc6249ef79c6bfc196f8cc15016eda64
Doridian <git@doridian.net>
2026-03-12 22:04:03 -0700

    mh

--- nix/modules/nixos/services/broadcast-box/sdpfixer.js
+++ nix/modules/nixos/services/broadcast-box/sdpfixer.js
@@ -3,11 +3,11 @@ async function fixup(r, path) {
         method: r.method,
     });
     if (res.status < 200 || res.status > 299) {
-        r.return(res.status, `Error fetching ${r.method} ${path}: ${res.responseBody}`);
+        r.return(res.status, `Error fetching ${r.method} ${path}: ${res.responseText}`);
         return;
     }
 
-    const data = res.responseBody
+    const data = res.responseText
         .replace(/rport [0-9]+/g, 'rport 3333')
         .replace(/[0-9]+ typ srflx/g, '3333 typ srflx');
 
