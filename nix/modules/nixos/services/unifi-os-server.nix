f8fd463bd1c9d5049456d74ceb18085747ad89a2
Doridian <git@doridian.net>
2026-03-24 20:44:49 -0700

    nah

--- nix/modules/nixos/services/unifi-os-server.nix
+++ nix/modules/nixos/services/unifi-os-server.nix
@@ -95,11 +95,10 @@ in
           '';
           serviceConfig = {
             ExecStartPre = [
-              "+-${pkgs.coreutils}/bin/rm -rf /sys/fs/cgroup/foxden-oci-${name}"
               "+${pkgs.coreutils}/bin/mkdir -p /sys/fs/cgroup/foxden-oci-${name}"
               "+${pkgs.coreutils}/bin/chown -R ${user.name}:${user.group} /sys/fs/cgroup/foxden-oci-${name}"
             ];
-            ExecStopPost = [ "+-${pkgs.coreutils}/bin/rm -rf /sys/fs/cgroup/foxden-oci-${name}" ];
+            ExecStopPost = [ "+-${pkgs.coreutils}/bin/rmdir /sys/fs/cgroup/foxden-oci-${name}" ];
           };
         };
       }).config
