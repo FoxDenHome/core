8ccb5805754bca8bf121a82062313f3d14c8fa4f
Doridian <git@doridian.net>
2026-04-02 23:49:05 -0700

    Try immich agai

---
+++ nix/systems/x86_64-linux/islandfox/immich.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+let
+  mkVlanHost = config.lib.foxDenSys.mkVlanHost;
+in
+{
+  foxDen.services = config.lib.foxDen.sops.mkIfAvailable {
+    immich = {
+      enable = true;
+      host = "immich";
+      tls.enable = false;
+    };
+  };
+
+  foxDen.hosts.hosts = {
+    immich = mkVlanHost 2 {
+      dns = {
+        fqdns = [ "images-demo.foxden.network" ];
+      };
+      addresses = [
+        "10.2.11.223/16"
+        "fd2c:f4cb:63be:2::ff17/64"
+      ];
+    };
+  };
+}
