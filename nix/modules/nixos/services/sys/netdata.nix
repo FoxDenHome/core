f2786fe6038fdaa1950b2b12a7c3a9e8296117a0
Doridian <git@doridian.net>
2026-04-02 22:20:05 -0700

    Monitor down services with netdata

--- nix/modules/nixos/services/sys/netdata.nix
+++ nix/modules/nixos/services/sys/netdata.nix
@@ -28,6 +28,38 @@ in
         withCloudUi = true;
         withIpmi = false;
       };
+
+      # https://www.netdata.cloud/blog/systemd-service-liveness/
+      configDir."go.d/systemdunits.conf" = pkgs.writeText "systemdunits.conf" ''
+        jobs:
+          - name: all-services
+            include:
+              - '*.service'
+      '';
+
+      configDir."health.d/systemdunits.conf" = pkgs.writeText "systemdunits-health.conf" ''
+         template: systemd_service_unit_failed_state
+               on: systemd.service_unit_state
+            class: Errors
+             type: Linux
+        component: Systemd units
+             calc: $failed
+            units: state
+            every: 10s
+             warn: $this != nan AND $this == 1
+            delay: down 5m multiplier 1.5 max 1h
+             info: systemd service unit in the failed state
+               to: sysadmin
+
+         template: systemd_service_unit_inactive_state
+               on: systemd.service_unit_state
+             calc: $inactive
+            every: 10s
+             warn: $this != nan AND $this == 1
+            delay: down 5m multiplier 1.5 max 1h
+             info: systemd service unit is inactive (possible restart loop)
+               to: sysadmin
+      '';
     };
     environment.persistence."/nix/persist/netdata" = {
       hideMounts = true;
