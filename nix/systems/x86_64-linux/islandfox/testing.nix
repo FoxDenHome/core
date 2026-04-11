91ee16daef99a7fe501f79eb76ea117292c06a2e
Doridian <git@doridian.net>
2026-04-11 01:56:45 -0700

    aaaa

---
+++ nix/systems/x86_64-linux/islandfox/testing.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+{
+  config.systemd.services.failtest = {
+    description = "A test service that fails to start";
+    serviceConfig = {
+      ExecStart = "${pkgs.coreutils}/bin/false";
+      Restart = "never";
+    };
+  };
+}
