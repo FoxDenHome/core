95c122bfd3362fb38ad3ad501d695049d9cae70f
Doridian <git@doridian.net>
2026-04-11 09:22:48 -0700

    Autoprune and autooptimize

--- nix/modules/nixos/nix-daemon.nix
+++ nix/modules/nixos/nix-daemon.nix
@@ -1,5 +1,22 @@
 { ... }:
 {
-  nix.daemonCPUSchedPolicy = "idle";
-  nix.daemonIOSchedClass = "idle";
+  config.nix = {
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedClass = "idle";
+    gc = {
+      automatic = true;
+      dates = [ "*-*-01 05:00:00" ];
+      randomizedDelaySec = "4h";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "*-*-04 05:00:00" ];
+      randomizedDelaySec = "4h";
+    };
+    settings = {
+      keep-derivations = true;
+      keep-outputs = true;
+      auto-optimise-store = true;
+    };
+  };
 }
