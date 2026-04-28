8b61cc3bacf3d7c6d4428d935f1556c86982f2a0
Doridian <git@doridian.net>
2026-04-28 16:02:39 -0700

    Try adding rmfakecloud

---
+++ nix/modules/nixos/services/rmfakecloud.nix
@@ -0,0 +1,57 @@
+{
+  foxDenLib,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  services = foxDenLib.services;
+
+  svcConfig = config.foxDen.services.rmfakecloud;
+  hostName = services.getFirstFQDN config svcConfig;
+  proto = if svcConfig.tls.enable then "https" else "http";
+in
+{
+  options.foxDen.services.rmfakecloud = {
+  }
+  // (services.http.mkOptions {
+    svcName = "restic-server";
+    name = "Restic Backup Server";
+  });
+
+  config = lib.mkIf svcConfig.enable (
+    lib.mkMerge [
+      (services.http.make {
+        inherit svcConfig pkgs config;
+        name = "http-rmfakecloud";
+        target = "proxy_pass http://127.0.0.1:3000;";
+      }).config
+      (services.make {
+        inherit svcConfig pkgs config;
+        name = "rmfakecloud";
+      }).config
+      {
+        sops.secrets.rmfakecloud = config.lib.foxDen.sops.mkIfAvailable { };
+
+        services.rmfakecloud = {
+          enable = true;
+          port = 3000;
+          storageUrl = "${proto}://${hostName}";
+          environmentFile = config.lib.foxDen.sops.mkIfAvailable config.sops.secrets.rmfakecloud.path;
+          extraSettings = {
+            DATADIR = "/var/lib/rmfakecloud";
+            RM_TRUST_PROXY = "true";
+            RM_HTTPS_COOKIE = if svcConfig.tls.enable then "true" else "";
+          };
+        };
+
+        systemd.services.rmfakecloud = {
+          serviceConfig = {
+            StateDirectory = "rmfakecloud";
+          };
+        };
+      }
+    ]
+  );
+}
---
+++ nix/systems/x86_64-linux/islandfox/rmfakecloud.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+let
+  mkVlanHost = config.lib.foxDenSys.mkVlanHost;
+in
+{
+  foxDen.services = config.lib.foxDen.sops.mkIfAvailable {
+    rmfakecloud = {
+      enable = true;
+      host = "rmfakecloud";
+      tls.enable = true;
+    };
+  };
+
+  foxDen.hosts.hosts = {
+    rmfakecloud = mkVlanHost 3 {
+      dns = {
+        fqdns = [ "rmfakecloud.foxden.network" ];
+      };
+      addresses = [
+        "10.3.10.7/16"
+        "fd2c:f4cb:63be:3::a07/64"
+      ];
+    };
+  };
+}
