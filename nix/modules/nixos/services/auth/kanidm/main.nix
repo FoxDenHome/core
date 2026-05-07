47f70ca5c8315032d9f0023301cf81f0660ba652
Doridian <git@doridian.net>
2026-05-07 07:50:53 -0700

    Kanidm 1.10

--- nix/modules/nixos/services/auth/kanidm/main.nix
+++ nix/modules/nixos/services/auth/kanidm/main.nix
@@ -3,7 +3,7 @@
   services.kanidm = {
     enableClient = true;
 
-    package = pkgs.kanidmWithSecretProvisioning_1_9;
+    package = pkgs.kanidmWithSecretProvisioning_1_10;
 
     clientSettings = {
       uri = "https://auth.foxden.network";
