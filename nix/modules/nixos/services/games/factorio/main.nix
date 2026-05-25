f1c6d28fe28db401efd307a6e24932f3d44a49f2
Doridian <git@doridian.net>
2026-05-24 22:10:52 -0700

    try

--- nix/modules/nixos/services/games/factorio/main.nix
+++ nix/modules/nixos/services/games/factorio/main.nix
@@ -49,7 +49,7 @@ in
                 (derivation {
                   name = "${modInfo.name}-${modInfo.version}.zip";
                   builder = pkgs.writeShellScript "download-mod.sh" ''
-                    ${pkgs.wget}/bin/wget -q -O "$out" "$1?$FACTORIO_AUTH"
+                    ${pkgs.wget}/bin/wget -O "$out" "$1?$FACTORIO_AUTH"
                   '';
                   args = [
                     modInfo.url
