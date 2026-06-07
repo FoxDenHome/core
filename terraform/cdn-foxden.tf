24929585db81917ade02743eefdf214d239f1f55
Doridian <git@doridian.net>
2026-06-06 20:06:11 -0700

    Rename to CDN

--- terraform/cdn-foxden.tf
+++ terraform/cdn-foxden.tf
@@ -1,15 +1,15 @@
-resource "cloudns_dns_record" "ping_foxden" {
+resource "cloudns_dns_record" "cdn_foxden" {
   zone = "foxden.network"
 
   type  = "CNAME"
   ttl   = 3600
-  name  = "ping"
+  name  = "cdn"
   value = "x.sni.global.fastly.net"
 }
 
 output "generated_records" {
   value = {
-    "foxden.network" = [for r in setunion([cloudns_dns_record.ping_foxden]) : {
+    "foxden.network" = [for r in setunion([cloudns_dns_record.cdn_foxden]) : {
       type     = upper(r.type)
       fqdn     = r.name == "@" ? r.zone : "${r.name}.${r.zone}"
       name     = r.name
