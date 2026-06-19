a21b4d1191fb830e029407185ba3ea6e98ad1b1d
Doridian <git@doridian.net>
2026-06-18 19:23:08 -0700

    aaa

--- nix/modules/lib/global/dns.nix
+++ nix/modules/lib/global/dns.nix
@@ -171,21 +171,21 @@ let
             fqdn = "fm1._domainkey.${fqdn}";
             type = "CNAME";
             ttl = 3600;
-            value = "fm1.${fqdn}.dkim.fmhosted.com";
+            value = "fm1.${fqdn}.dkim.fmhosted.com.";
             horizon = "*";
           }
           {
             fqdn = "fm2._domainkey.${fqdn}";
             type = "CNAME";
             ttl = 3600;
-            value = "fm2.${fqdn}.dkim.fmhosted.com";
+            value = "fm2.${fqdn}.dkim.fmhosted.com.";
             horizon = "*";
           }
           {
             fqdn = "fm3._domainkey.${fqdn}";
             type = "CNAME";
             ttl = 3600;
-            value = "fm3.${fqdn}.dkim.fmhosted.com";
+            value = "fm3.${fqdn}.dkim.fmhosted.com.";
             horizon = "*";
           }
         ]
