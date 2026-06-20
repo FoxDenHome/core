a4ae0312edd3a3997363e675528023c21ae898dd
Doridian <git@doridian.net>
2026-06-19 19:56:22 -0700

    And now no more onboot config at all, which makes netwatch much happier

--- mikrotik/configure/vrrp.py
+++ mikrotik/configure/vrrp.py
@@ -1,15 +1,27 @@
-from configure.util import MTikScript, ROUTERS
+from configure.util import MTikRouter, MTikScript, ROUTERS, mtik_path
+
+MAIN_SCRIPT = "wan-online-adjust"
+TEMPLATE = mtik_path(f"files/vrrp/{MAIN_SCRIPT}.rsc")
+
+def make_vrrp_script(router: MTikRouter) -> None:
+    with open(TEMPLATE, "r") as file:
+        data = file.read()
+
+    data = data.replace("# VRRP PRIORITY ONLINE #", str(router.vrrp_priority_online))
+    data = data.replace("# VRRP PRIORITY OFFLINE #", str(router.vrrp_priority_offline))
+
+    router.scripts.add(
+        MTikScript(
+            name=MAIN_SCRIPT,
+            source=data,
+            schedule="1m",
+        )
+    )
 
 
 def refresh_vrrp():
     for router in ROUTERS:
+        if router.horizon != "internal":
+            continue
         print(f"## {router.host}")
-        router.scripts.add(
-            MTikScript(
-                name="onboot-vrrp-config",
-                source=f":global VRRPPriorityOnline {router.vrrp_priority_online}\n"
-                + f":global VRRPPriorityOffline {router.vrrp_priority_offline}\n",
-                run_on_change=True,
-                schedule="startup",
-            )
-        )
+        make_vrrp_script(router)
--- mikrotik/files/vrrp/wan-online-adjust.rsc
+++ mikrotik/files/vrrp/wan-online-adjust.rsc
@@ -1,21 +1,16 @@
-# dont-require-permissions=true
-# policy=read,write,policy,test
-# schedule=1m
+# This script may not depend on globals as netwatch doesn't seem to be able to see them...
 
-:global logputwarning
-
-:global VRRPPriorityOffline
-:global VRRPPriorityOnline
-:local VRRPPriorityCurrent $VRRPPriorityOffline
+:local VRRPPriorityCurrent # VRRP PRIORITY OFFLINE #
 
 if ([/system/script/find name=local-maintenance-mode ]) do={
-    $logputwarning "Maintenance mode ON"
+    :log warning "Maintenance mode ON"
+    :put "Maintenance mode ON"
 } else={
     :local defgwidx [ /ip/route/find dynamic active dst-address=0.0.0.0/0 ]
     if ([:len $defgwidx] > 0) do={
         :local status [ /tool/netwatch/get [ /tool/netwatch/find comment="monitor-default" ] status ]
         if ($status = "up") do={
-            :set VRRPPriorityCurrent $VRRPPriorityOnline
+            :set VRRPPriorityCurrent # VRRP PRIORITY ONLINE #
         }
     }
 }
