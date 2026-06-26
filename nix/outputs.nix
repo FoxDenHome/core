0ca8efda6f790f15cfcabff14a7171a6bf5f1a34
Doridian <git@doridian.net>
2026-06-26 09:08:55 -0700

    Add CUDA cache

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -168,11 +168,19 @@ let
                 crossSystem.system = "${arch}-linux";
                 config.allowUnfree = true;
               };
-              nix.settings.experimental-features = [
-                "configurable-impure-env"
-                "nix-command"
-                "flakes"
-              ];
+              nix.settings = {
+                experimental-features = [
+                  "configurable-impure-env"
+                  "nix-command"
+                  "flakes"
+                ];
+                substituters = [
+                  "https://cache.nixos-cuda.org"
+                ];
+                trusted-public-keys = [
+                  "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+                ];
+              };
               environment.systemPackages = with pkgs; [
                 arch-install-scripts
                 git
