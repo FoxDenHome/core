245db551854d62322a8dc9ed515b2439ad866ebc
Doridian <git@doridian.net>
2026-07-16 11:48:02 -0700

    not dynamic

--- nix/modules/nixos/services/affine.nix
+++ nix/modules/nixos/services/affine.nix
@@ -58,9 +58,9 @@ in
             Environment = [
               "UPLOAD_LOCATION=/var/lib/affine/data"
               "CONFIG_LOCATION=/var/lib/affine/config"
-              "\"DATABASE_URL=postgresql://affine@${
+              "DATABASE_URL=postgresql://affine@${
                 lib.replaceString "/" "%2F" config.foxDen.services.postgresql.socketPath
-              }/affine\""
+              }/affine"
               "AFFINE_INDEXER_ENABLED=false"
             ];
             ExecStart = [ "${pkgs.affine-server}/bin/affine-server" ];
