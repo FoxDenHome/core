9037baa632aa819b4b6996b0f46491691811138a
Doridian <git@doridian.net>
2026-07-15 20:10:43 -0700

    Try adding affine

---
+++ nix/modules/nixos/services/affine.nix
@@ -0,0 +1,57 @@
+{
+  foxDenLib,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  services = foxDenLib.services;
+  svcConfig = config.foxDen.services.affine;
+in
+{
+  options.foxDen.services.affine = services.http.mkOptions {
+    name = "AFFiNE";
+  };
+
+  config = lib.mkIf svcConfig.enable (
+    lib.mkMerge [
+      (services.http.make {
+        inherit svcConfig pkgs config;
+        name = "http-affine";
+        target = "proxy_pass http://127.0.0.1:3000;";
+      }).config
+      (services.make {
+        inherit svcConfig pkgs config;
+        name = "affine";
+      }).config
+      {
+        sops.secrets.affine = config.lib.foxDen.sops.mkIfAvailable { };
+
+        foxDen.services.postgresql = {
+          enable = true;
+          services = [
+            {
+              database = "affine";
+              service = "affine";
+            }
+          ];
+        };
+
+        systemd.services.affine = {
+          serviceConfig = {
+            DynamicUser = true;
+            StateDirectory = "affine";
+            Environment = [
+              "UPLOAD_LOCATION=/var/lib/affine/data"
+              "CONFIG_LOCATION=/var/lib/affine/config"
+              "DATABASE_URL=postgresql://affine@${lib.replaceString "/" "%2F" config.foxDen.services.postgresql.socketPath}/affine"
+              "AFFINE_INDEXER_ENABLED=false"
+            ];
+          };
+          wantedBy = [ "multi-user.target" ];
+        };
+      }
+    ]
+  );
+}
---
+++ nix/systems/x86_64-linux/islandfox/affine.nix
@@ -0,0 +1,28 @@
+{ config, ... }:
+let
+  mkVlanHost = config.lib.foxDenSys.mkVlanHost;
+in
+{
+  foxDen.services = config.lib.foxDen.sops.mkIfAvailable {
+    affine = {
+      enable = true;
+      host = "affine";
+    };
+  };
+
+  foxDen.hosts.hosts = {
+    affine = mkVlanHost 2 {
+      dns = {
+        fqdns = [
+          "docs.foxden.network"
+        ];
+        dynDns = true;
+      };
+      webservice.enable = true;
+      addresses = [
+        "10.2.11.7/16"
+        "fd2c:f4cb:63be:2::b07/64"
+      ];
+    };
+  };
+}
