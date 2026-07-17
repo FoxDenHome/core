1e95d1273615a38af6cb7038d6a199ff4b2dd4b8
Doridian <git@doridian.net>
2026-07-17 08:16:45 -0700

    chmod 700 affine

--- nix/modules/nixos/services/affine.nix
+++ nix/modules/nixos/services/affine.nix
@@ -122,6 +122,7 @@ in
             EnvironmentFile = config.lib.foxDen.sops.mkIfAvailable config.sops.secrets.affine.path;
             ExecStartPre = [ "${pkgs.writeShellScript "affine-server-init.sh" ''
               ${pkgs.coreutils}/bin/mkdir -p /var/lib/affine/.affine/{config,storage}
+              ${pkgs.coreutils}/bin/chmod 700 /var/lib/affine /var/lib/affine/.affine{,/config,/storage}
               ${pkgs.coreutils}/bin/cp -fv ${builtins.toFile "config.json" (builtins.toJSON cfgJson)} /var/lib/affine/.affine/config/config.json
             ''}"];
             ExecStart = [ "${pkgs.affine-server}/bin/affine-server" ];
