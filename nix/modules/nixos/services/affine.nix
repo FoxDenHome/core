7ce9809e1519a22ad28107b3e1278ff084bcf792
Doridian <git@doridian.net>
2026-07-17 07:17:52 -0700

    Add mail settings to affine

--- nix/modules/nixos/services/affine.nix
+++ nix/modules/nixos/services/affine.nix
@@ -23,7 +23,7 @@ let
     };
     mailer = {
       "SMTP.name" = hostName;
-      "SMTP.sender" = "FoxDen Docs <docs@${hostName}>";
+      "SMTP.sender" = "FoxDen Docs <docs@$foxden.network>";
     };
     telemetry = {
       allowedOrigin = [ externalUrl ];
