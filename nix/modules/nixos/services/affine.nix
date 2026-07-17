402b61af057db2796be96f692532de5b0eab72eb
Doridian <git@doridian.net>
2026-07-17 06:53:48 -0700

    No more debug

--- nix/modules/nixos/services/affine.nix
+++ nix/modules/nixos/services/affine.nix
@@ -103,7 +103,7 @@ in
 
           environment = {
             DATABASE_URL = "postgres://affine@localhost/affine?host=/run/postgresql/";
-            DEBUG_LOGGING = "true";
+            DEBUG_LOGGING = "false";
           };
 
           serviceConfig = {
