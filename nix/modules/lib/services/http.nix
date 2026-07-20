e68e315245d8b745e45552b75056f8667d9e8234
Doridian <git@doridian.net>
2026-07-20 16:58:56 -0700

    Backend too old lul

--- nix/modules/lib/services/http.nix
+++ nix/modules/lib/services/http.nix
@@ -452,7 +452,7 @@ in
         proxy_http_version 1.1;
         proxy_request_buffering off;
         proxy_buffering off;
-        proxy_read_timeout 86400s;
+        proxy_read_timeout 60s;
         fastcgi_request_buffering off;
         fastcgi_buffering off;
         client_max_body_size 0;
