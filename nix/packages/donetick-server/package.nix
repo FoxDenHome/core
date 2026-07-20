593b549ca1e1bf6a4b60d45000c7b3265084d7c2
Doridian <git@doridian.net>
2026-07-20 16:53:06 -0700

    Backend too old lul

--- nix/packages/donetick-server/package.nix
+++ nix/packages/donetick-server/package.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 let
-  version = "0.1.75";
+  version = "0.1.76-beta.20";
 in
 pkgs.buildGoModule {
   pname = "donetick-server";
@@ -9,7 +9,7 @@ pkgs.buildGoModule {
     repo = "donetick";
     owner = "donetick";
     rev = "v${version}";
-    hash = "sha256-UbL/bvh0tSxtYKIL83zsZs/PVLwlAaGqiMKy7/hQD/s=";
+    hash = "sha256-2dR1h1iRLDUorrcnp42PTR62m1OPz2ZcGw/FZBZF670=";
   };
   vendorHash = "sha256-4Ho9lIWk80k+6wVCk27EPYdD7eDC0SUXR9PcIAVmBRA=";
 
