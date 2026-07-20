34a1b06e0efea26707903957c7596d4579bc2dc1
Doridian <git@doridian.net>
2026-07-20 12:18:55 -0700

    Add package for donetick

---
+++ nix/packages/donetick-server/package.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... } :
+let
+  version = "0.1.75";
+in 
+pkgs.buildGoModule {
+  pname = "donetick-server";
+  inherit version;
+  src = pkgs.fetchFromGitHub {
+    repo = "donetick";
+    owner = "donetick";
+    rev = "v${version}";
+    hash = "sha256-UbL/bvh0tSxtYKIL83zsZs/PVLwlAaGqiMKy7/hQD/s=";
+  };
+  vendorHash = builtins.readFile ./vendor-hash.txt;
+
+  buildInputs = [ ];
+  ldflags = [ "-s -w" ];
+
+  postInstall = ''
+    mv $out/bin/core $out/bin/donetick-server
+  '';
+}
