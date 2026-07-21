17c5f9ec1d2115a4545a28a9e1f797e5dd336414
Doridian <git@doridian.net>
2026-07-20 21:57:28 -0700

    i want name

--- nix/modules/nixos/services/donetick.nix
+++ nix/modules/nixos/services/donetick.nix
@@ -49,7 +49,7 @@ let
             "openid"
             "profile"
             "email"
-            "groups"
+            "groups_name"
           ];
           auth_url = "https://auth.foxden.network/ui/oauth2";
           token_url = "https://auth.foxden.network/oauth2/token";
