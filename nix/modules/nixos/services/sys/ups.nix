d191ec9a1c6d1abff9c960d834193aefd6ee3665
Doridian <git@doridian.net>
2026-07-23 13:41:22 -0700

    automatic passwords

--- nix/modules/nixos/services/sys/ups.nix
+++ nix/modules/nixos/services/sys/ups.nix
@@ -59,10 +59,12 @@ in
       ups.ups-rack = {
         driver = "snmp-ups";
         port = "ups-rack.foxden.network:161";
-        community = "monitor_sprFp7";
-        snmp_version = "v1";
-        mibs = "pw";
-        "override.battery.charge.low" = svcConfig.lowBatteryLevel;
+        directives = {
+          community = "monitor_sprFp7";
+          snmp_version = "v1";
+          mibs = "pw";
+          "override.battery.charge.low" = svcConfig.lowBatteryLevel;
+        };
       };
       users = {
         upsmon = {
