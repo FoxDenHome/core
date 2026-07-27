137d79db17b5bb36688dbd97aa19b1ae914d72d4
Doridian <git@doridian.net>
2026-07-27 14:58:42 -0700

    Set gentoo

--- nix/modules/nixos/base.nix
+++ nix/modules/nixos/base.nix
@@ -109,7 +109,10 @@
     fish.enable = true;
     zsh = {
       enable = true;
-      ohMyZsh.enable = true;
+      ohMyZsh = {
+        enable = true;
+        theme = "gentoo";
+      };
     };
     git.enable = true;
     htop.enable = true;
