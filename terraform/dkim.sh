a7ce1a7a9e474c317abc41e28fa88f03a6e2199b
Doridian <git@doridian.net>
2026-08-07 15:04:51 -0700

    Add foxmail on icefox stuff

--- terraform/dkim.sh
+++ terraform/dkim.sh
@@ -2,4 +2,5 @@
 exec curl -fsL \
         http://router.foxden.network:9002/dkim.json \
         http://router-backup.foxden.network:9002/dkim.json \
+        http://icefox-foxmail.foxden.network:9002/dkim.json \
     | jq -s '.[0] * .[1]'
