b94842211a61550dd3bce8663632abe980c302f4
Doridian <git@doridian.net>
2026-08-07 21:27:20 -0700

    ohno

--- terraform/dkim.tf
+++ terraform/dkim.tf
@@ -7,7 +7,7 @@ resource "dns-he-net_txt" "dkim" {
   zone_id  = local.he_zone_ids[replace(each.key, "/^.*\\._domainkey\\./", "")]
   for_each = data.external.dkim_json.result
 
-  domain = replace(each.key, "/^.*\\._domainkey\\./", "")
+  domain = each.key
   ttl    = 3600
   data   = "\"${each.value}\""
 }
