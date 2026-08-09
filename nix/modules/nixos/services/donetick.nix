afab6e4aaec77563ebae97d5eb4c66ccf8a5638b
Doridian <git@doridian.net>
2026-08-09 16:04:59 -0700

    Always allow PW to donetick

--- nix/modules/nixos/services/donetick.nix
+++ nix/modules/nixos/services/donetick.nix
@@ -16,7 +16,7 @@ let
   cfgJYaml = {
     single_circle_instance = true;
     is_user_creation_disabled = true;
-    disable_password_auth = svcConfig.oAuth.enable;
+    disable_password_auth = false; # TODO: Until mobile OAuth works, then: svcConfig.oAuth.enable;
 
     server = {
       port = 3010;
