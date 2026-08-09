497b4580f39f25070605ff06671205e093a48676
Doridian <git@doridian.net>
2026-08-09 07:47:25 -0700

    Update MC

--- nix/modules/nixos/services/sys/hugepages.nix
+++ nix/modules/nixos/services/sys/hugepages.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
   systemd.tmpfiles.rules = [
-    "w /sys/kernel/mm/transparent_hugepage/shmem_enabled 0644 root root 10d advise"
+    "w /sys/kernel/mm/transparent_hugepage/shmem_enabled 0644 root root - advise"
   ];
 }
