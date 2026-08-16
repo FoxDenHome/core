7c8cb3169a59a04c731dde3b6756ca3dd5c0e953
Doridian <git@doridian.net>
2026-08-16 11:53:16 -0700

    Forget about it, storcli is annoying, controller indices arent even stable

--- nix/modules/nixos/services/monitoring/prometheus/node-exporter.nix
+++ nix/modules/nixos/services/monitoring/prometheus/node-exporter.nix
@@ -7,27 +7,29 @@
 {
   options.foxDen.node-exporter = {
     enable = lib.mkEnableOption "Enable Prometheus node-exporter";
-    textfilesContrib = lib.mkOption {
-      readOnly = true;
-      default = pkgs.fetchFromGitHub {
-        owner = "prometheus-community";
-        repo = "node-exporter-textfile-collector-scripts";
-        rev = "53d484331170e6f00237f0fbd6f4acae33064b87";
-        hash = "sha256-mKu61Ds3B/ChmfbEqqFc2ZjBbly3jYCSNZwD3cF5eHg=";
+    textfile = {
+      scripts = lib.mkOption {
+        readOnly = true;
+        default = pkgs.fetchFromGitHub {
+          owner = "prometheus-community";
+          repo = "node-exporter-textfile-collector-scripts";
+          rev = "53d484331170e6f00237f0fbd6f4acae33064b87";
+          hash = "sha256-mKu61Ds3B/ChmfbEqqFc2ZjBbly3jYCSNZwD3cF5eHg=";
+        };
+        type = lib.types.package;
       };
-      type = lib.types.package;
-    };
-    python = lib.mkOption {
-      readOnly = true;
-      default = pkgs.python3.withPackages (pypkgs: [
-        pypkgs.prometheus-client
-      ]);
-      type = lib.types.package;
-    };
-    textfileDirectory = lib.mkOption {
-      readOnly = true;
-      type = lib.types.str;
-      default = "/run/node-exporter-textfile";
+      python = lib.mkOption {
+        readOnly = true;
+        default = pkgs.python3.withPackages (pypkgs: [
+          pypkgs.prometheus-client
+        ]);
+        type = lib.types.package;
+      };
+      directory = lib.mkOption {
+        readOnly = true;
+        type = lib.types.str;
+        default = "/run/node-exporter-textfile";
+      };
     };
   };
 
