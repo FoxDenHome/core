1228cd24d986ba5ddc6e8b45145308102f17afd6
Doridian <git@doridian.net>
2026-08-16 19:36:53 -0700

    add sas3temp

--- nix/modules/nixos/hw/storage/lsi.nix
+++ nix/modules/nixos/hw/storage/lsi.nix
@@ -11,5 +11,29 @@
     environment.systemPackages = with pkgs; [
       storcli
     ];
+
+    systemd.services.storcli-node-exporter = {
+      serviceConfig = {
+        Type = "simple";
+        User = "root";
+        Group = "root";
+        ExecStart = [
+          "${pkgs.writeShellScript "sas3temp-node-exporter.sh" ''
+            set -euo pipefail
+            outfile='${config.foxDen.node-exporter.textfileDirectory}/sas3temp.prom'
+            ${pkgs.sas3temp}/bin/sas3temp > "$outfile.tmp"
+            rm -f "$outfile"
+            mv "$outfile.tmp" "$outfile"
+          ''}"
+        ];
+      };
+    };
+
+    systemd.timers.storcli-node-exporter = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-* *:*:13";
+      };
+    };
   };
 }
