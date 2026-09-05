4a0d4be2c43af9b7509bc39cbd0cfdf11d2f7419
Doridian <git@doridian.net>
2026-09-05 15:36:59 -0700

    Disable sage

--- nix/modules/nixos/services/games/factorio/main.nix
+++ nix/modules/nixos/services/games/factorio/main.nix
@@ -32,7 +32,7 @@ in
           game-name = "FoxDen Factorio";
           description = "FoxDen Factorio";
 
-          package = pkgs.factorio-headless.overrideAttrs (old: {
+          package = pkgs.factorio-headless-experimental.overrideAttrs (old: {
             installPhase = old.installPhase + ''
               rm -r $out/share/factorio/data/{elevated-rails,quality,space-age}
             '';
