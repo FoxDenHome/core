1d697f46b20e37d32274e32453c519941835f5c3
Doridian <git@doridian.net>
2026-09-06 15:17:32 -0700

    COEP entire /game/

--- nix/modules/nixos/services/games/darksignsonline.nix
+++ nix/modules/nixos/services/games/darksignsonline.nix
@@ -74,8 +74,12 @@ in
             location = /game {
               return 308 /game/;
             }
+            location = /game/ {
+              alias /var/www/darksignsonline/game/index.html;
+              ${headerConfig}
+              add_header Cross-Origin-Embedder-Policy "require-corp" always;
+            }
             location /game/ {
-              index index.html;
               ${headerConfig}
               add_header Cross-Origin-Embedder-Policy "require-corp" always;
             }
