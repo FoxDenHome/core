a10c346ed11f8c6ce23a5eb894e67bd12129712b
Doridian <git@doridian.net>
2026-09-09 15:42:14 -0700

    Allow ubuntu

--- nix/modules/nixos/services/cghmn/squidcache.nix
+++ nix/modules/nixos/services/cghmn/squidcache.nix
@@ -91,7 +91,7 @@ in
       };
       domains = lib.mkOption {
         type = uniq (listOf str);
-        default = [ ".debian.org" ];
+        default = [ ".debian.org" ".ubuntu.com" ];
         description = "Which domains to permit proxying for";
       };
     }
