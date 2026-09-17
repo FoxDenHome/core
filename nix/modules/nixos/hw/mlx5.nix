4d10f4180010892b8690584ad1e0f1fc15425481
Doridian <git@doridian.net>
2026-09-16 18:43:27 -0700

    Always set mlx5 udev rules

--- nix/modules/nixos/hw/mlx5.nix
+++ nix/modules/nixos/hw/mlx5.nix
@@ -1,28 +1,43 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   yes = lib.kernel.yes;
+  ethtool = "${pkgs.ethtool}/bin/ethtool";
 in
 {
   options.foxDen.mlx5.enable = lib.mkEnableOption "Enable full MLX5 kernel support";
 
-  config = lib.mkIf config.foxDen.mlx5.enable {
-    boot.kernelModules = [
-      "mlx5_core"
-      "mlx5_en"
-      "rdma_cm"
-    ];
-    boot.kernelPatches = [
-      {
-        name = "mlx5-en-tls";
-        patch = null;
-        structuredExtraConfig = {
-          MLX5_FPGA = yes;
-          MLX5_EN_TLS = yes;
-          MLX5_CORE_IPOIB = yes;
-          MLX5_MACSEC = yes;
-          MLX5_EN_IPSEC = yes;
-        };
-      }
-    ];
-  };
+  config = lib.mkMerge [
+    {
+      services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="net", DRIVERS=="mlx5_core", \
+          RUN+="${ethtool} --set-priv-flags $name rx_cqe_compress off", \
+          RUN+="${ethtool} -K $name rxhash on"
+      '';
+    }
+    (lib.mkIf config.foxDen.mlx5.enable {
+      boot.kernelModules = [
+        "mlx5_core"
+        "mlx5_en"
+        "rdma_cm"
+      ];
+      boot.kernelPatches = [
+        {
+          name = "mlx5-en-tls";
+          patch = null;
+          structuredExtraConfig = {
+            MLX5_FPGA = yes;
+            MLX5_EN_TLS = yes;
+            MLX5_CORE_IPOIB = yes;
+            MLX5_MACSEC = yes;
+            MLX5_EN_IPSEC = yes;
+          };
+        }
+      ];
+    })
+  ];
 }
