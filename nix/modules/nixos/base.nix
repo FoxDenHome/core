545b6bcb10a324dbc5f32b9816bc70d509a1d882
Doridian <git@doridian.net>
2026-09-17 18:14:00 -0700

    Persist timesync and randomseed

--- nix/modules/nixos/base.nix
+++ nix/modules/nixos/base.nix
@@ -184,7 +184,9 @@
         "/var/lib/nixos"
         "/var/lib/systemd/coredump"
         "/var/lib/systemd/pstore"
+        "/var/lib/systemd/random-seed"
         "/var/lib/systemd/timers"
+        "/var/lib/systemd/timesync"
         "/var/cache/fwupd"
         "/var/lib/fwupd"
       ];
