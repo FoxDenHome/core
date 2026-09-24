beee193350e13472fd72ab6d90ac49fdce56d1e3
Doridian <git@doridian.net>
2026-09-24 16:41:59 -0700

    route direct

--- nix/systems/x86_64-linux/icefox/network.nix
+++ nix/systems/x86_64-linux/icefox/network.nix
@@ -109,6 +109,7 @@ in
             routes = [
               {
                 Destination = "::/0";
+                GatewayOnLink = true;
                 Gateway = "2607:5300:60:70ff:ff:ff:ff:ff";
               }
             ];
